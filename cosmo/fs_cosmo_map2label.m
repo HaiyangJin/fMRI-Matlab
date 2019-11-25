d07c335488b5bb3806b99210e1d1568c8b8c625a
Haiyang <haiyang.jin@nyu.edu>
2019-11-25 16:36:15 +0400

    create function to save the searchlight results as FS label
    save as FreeSurfer label

---
+++ cosmo/fs_cosmo_map2label.m
@@ -0,0 +1,24 @@
+function fs_cosmo_map2label(dt_searchligh, filename, vert_coordi, subjCode)
+% This function convert the surface dataset in CoSMoMVPA to label file in
+% FreeSurfer.
+%
+% Created by Haiyang Jin (25/11/2019)
+
+[~, fn, ext] = fileparts(filename);
+if ~strcmp(ext, '.label')
+    filename = [filename '.label'];
+end
+
+acc = dt_searchligh.samples;
+nver = numel(acc);
+
+fid = fopen(filename, 'w');
+fprintf(fid, '#!ascii label Converted from CosmoMVPA. subject-%s coords=surface\n', subjCode);
+fprintf(fid, '%d\n', nver);
+for vID = 1:nver
+    fprintf(fid, '%d %5.3f %5.3f %5.3f %f\n', vID-1, vert_coordi(vID, 1), ...
+        vert_coordi(vID, 2), vert_coordi(vID, 3), acc(vID));  
+end
+fclose(fid);
+fprintf('Saved %s label for %s.\n', fn, subjCode);
+end
