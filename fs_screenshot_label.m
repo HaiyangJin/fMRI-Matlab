2f7585bcded384090aeffd573899138d089db760
Haiyang <haiyang.jin@nyu.edu>
2019-12-02 10:15:14 +0400

    function enhanced for visualizing multiple labels at the same time
    adding output (if the function runs successfully) Failure mainly is due to the non-existence of the labels
    
    could visualize multiple labels at the same time

--- fs_screenshot_label.m
+++ fs_screenshot_label.m
@@ -1,56 +1,98 @@
-function fs_screenshot_label(subjCode, fn_label, path_output, file_overlay)
+function isok = fs_screenshot_label(subjCode, fn_label, path_output, file_overlay, color_label)
 % This function takes the screenshot of the label based with specific 
 % contrast if there is. 
+%
+% file_overlay: the functional file (beta, or sig file)
 % 
 % Created by Haiyang Jin (28/11/2019)
+% Updated by Haiyang Jin (1/12/2019) Plot multiple labels (same hemisphere)
+
+isok = 1;
+
+% number of labels 
+if ischar(fn_label)
+    fn_label = {fn_label};
+end
+nLabel = numel(fn_label); % number of labels for visualization
 
 if nargin < 3 || isempty(path_output)
     path_output = '.';
 end
+if ~exist(path_output, 'dir'); mkdir(path_output); end % create the folder if necessary
 
-if ~exist(path_output, 'dir')
-    mkdir(path_output);
-end
-
-isContrast = 1;
+% create the freesurfer commands for the functional data
 if nargin < 4 || isempty(file_overlay)
-    isContrast = 0;
+    fscmd_overlay = '';
+else
+    fscmd_overlay = sprintf([':overlay=%s:overlay_threshold=2,4 '...
+        '-colorscale'],...
+        file_overlay);
 end
 
+% colors used for labels
+if nargin < 5 || isempty(color_label)
+    color_label = {'#FFFFFF', '#33cc33'}; % white, green
+end
+
+% FreeSurfer setup 
 FS = fs_setup;
-hemi = fs_hemi(fn_label);
 
+% make sure all labels are for the same hemisphere
+[hemi, nHemi] = fs_hemi_multi(fn_label);
+if nHemi ~= 1
+    error('Please make sure all labels used are for the same hemisphere.');
+end
+
+% surface files and the annotation file
 subjPath = fullfile(FS.subjects, subjCode);
-
-% files to be used in the command
 file_inf = fullfile(subjPath, 'surf', [hemi '.inflated']); % inflated file
 file_annot = fullfile(subjPath, 'label', [hemi '.aparc.annot']); % annotation file
-file_label = fullfile(subjPath, 'label', fn_label); % label file
 
-fscmd1 = sprintf(['freeview -f %s:'... % the ?h.inflated file
+fscmd_surf = sprintf(['freeview -f %s:'... % the ?h.inflated file
     'annot=%s:annot_outline=yes:'... % the filename and settings for annotation file
-    'label=%s:label_outline=yes:label_color=#FFFFFF'],...% the label file and settings
-    file_inf, file_annot, file_label);
+    ],...% the label file and settings
+    file_inf, file_annot);
 
-if isContrast
-    fscmd2 = sprintf([':overlay=%s:overlay_threshold=2,4 '...
-         '-colorscale'],...
-         file_overlay);
-else
-    fscmd2 = '';
+% the label files
+fscmd_label = '';
+for iLabel = 1:nLabel
+    
+    theLabel = fn_label{iLabel};
+    file_thelabel = fullfile(subjPath, 'label', theLabel); % label file
+    color_thelabel = color_label{iLabel};
+    
+    % make sure there is the label for this subject
+    isAvailable = fw_checklabel(theLabel, subjCode);
+    if ~isAvailable
+        isok = 0;
+        warning('Cannot find label %s for subject %s.\n', theLabel, subjCode);
+        return; % quit this function now
+    end
+    
+    fscmd_thislabel = sprintf('label=%s:label_outline=yes:label_color=%s:',...
+        file_thelabel, color_thelabel);
+    
+    fscmd_label = [fscmd_label fscmd_thislabel]; %#ok<AGROW>
 end
 
+% the camera angle
 isLeft = strcmp(hemi, 'lh');
 angle = 240 + 60 * isLeft;
-fn_output = sprintf('%s-%s.png', fn_label, subjCode);
-file_output = fullfile(path_output, fn_output); 
-fscmd3 = sprintf(' -cam dolly 1.5 elevation %d -ss %s', angle, file_output); %
+fscmd_camera = sprintf(' -cam dolly 1.5 elevation %d', angle);
 
-fscmd = [fscmd1 fscmd2 fscmd3];
+% the filename of the screenshot
+name_labels = sprintf(repmat('%s:', 1, nLabel), fn_label{:});
+name_labels = erase(name_labels, {'roi.', '.label'}); % shorten filenames
+fn_output = sprintf('%s%s.png', name_labels, subjCode);
+file_output = fullfile(path_output, fn_output);
+fscmd_output = sprintf(' -ss %s', file_output); %
 
+% combine the command together
+fscmd = [fscmd_surf fscmd_label fscmd_overlay fscmd_camera fscmd_output];
+
+% run the freesurfer command
 system(fscmd);
-
 fprintf('\nSave the screenshot of %s for %s successfully at %s\n\n', ...
-    fn_label, subjCode, file_output);
+    name_labels, subjCode, file_output);
 
 end
