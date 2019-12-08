317d4d5378208d10e86c6b0761507836fd2f4e80
Haiyang <haiyang.jin@nyu.edu>
2019-12-08 15:09:42 +0400

    add hemisphere info

--- fs_setup.m
+++ fs_setup.m
@@ -3,3 +3,5 @@ function FS=fs_setup
 % get the enviorment variables on FreeSurfer
 FS.homedir = getenv('FREESURFER_HOME');
 FS.subjects = getenv('SUBJECTS_DIR');
+FS.hemis = {'lh', 'rh'};
+FS.nHemi = numel(FS.hemis);
