f87d36565eb4c47af74e36c1a19314bd789ef762
Haiyang <haiyang.jin@nyu.edu>
2019-12-09 17:48:42 +0400

    add more fields
    test if FreeSurfer was setup properly
    add information of subject codes in $SUBJECTS_DIR

--- fs_setup.m
+++ fs_setup.m
@@ -1,7 +1,24 @@
-function FS=fs_setup
+function FS = fs_setup
+% This function gather some FreeSurfer information
 
 % get the enviorment variables on FreeSurfer
 FS.homedir = getenv('FREESURFER_HOME');
+
+if isempty(FS.homedir)
+    error('Please make sure you started Matlab in terminal after you setting up FreeSurfer properly.');
+end
+
 FS.subjects = getenv('SUBJECTS_DIR');
+
+% hemisphere information
 FS.hemis = {'lh', 'rh'};
 FS.nHemi = numel(FS.hemis);
+
+% subject code information
+subjdir = dir(FS.subjects);
+subjdir = subjdir([subjdir.isdir]);  % only keep folders
+FS.subjdir = subjdir(~ismember({subjdir.name}, {'.', '..'})); % remove . and ..
+FS.subjList = {FS.subjdir.name};
+FS.nSubj = numel(FS.subjList);
+
+end
