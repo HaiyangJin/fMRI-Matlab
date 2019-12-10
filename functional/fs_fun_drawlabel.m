7c211f5f8f92af3b1bb1fc5f0cb689d77b719809
Haiyang <haiyang.jin@nyu.edu>
2019-12-10 20:23:39 +0400

    get boldext from projStr

--- functional/fs_fun_drawlabel.m
+++ functional/fs_fun_drawlabel.m
@@ -1,33 +1,29 @@
-function fs_fun_drawlabel(projStr, contrast_name, boldext, siglevel, extraLabelInfo)
+function fs_fun_drawlabel(projStr, contrast_name, siglevel, extraLabelInfo)
 % This function use FreeSurfer ("tksurfer") to draw labels.
 %
 % Inputs: 
 %    projStr           matlab structure for the project (e.g.,fw_projectinfo).
 %                      This is specific for each project.
 %    contrast_name     contrast name used glm
-%    boldext           bold extension, part of the ananlysis name (usually use 'self')
 %    siglevel          significance level (default is f13 (.05))
 %    extraLabelInfo    extra label information added to the label name
 % Output:
-%    a label saved in that label folder
+%    a label saved in the label folder within $SUBJECTS_DIR
 %
 % Created by Haiyang Jin (10/12/2019)
 
-if nargin < 3 || isempty(boldext)
-    boldext = '_self';
-elseif ~strcmp(boldext(1), '_')
-    boldext = ['_' boldext];
-end
-if nargin < 4 || isempty(siglevel)
+
+if nargin < 3 || isempty(siglevel)
     siglevel = 'f13';
 end
-if nargin < 5 || isempty(extraLabelInfo)
+if nargin < 4 || isempty(extraLabelInfo)
     extraLabelInfo = '';
 elseif ~strcmp(extraLabelInfo(end), '.')
     extraLabelInfo = [extraLabelInfo, '.'];
 end
 
 % obtian the information about this bold type
+boldext = projStr.boldext;
 subjList = projStr.subjList;
 nSubj = projStr.nSubj;
 hemis = projStr.hemis;
