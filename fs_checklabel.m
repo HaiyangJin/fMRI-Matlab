6ad7c85af0a6b280c405194cf31d345f3f35bf22
Haiyang <haiyang.jin@nyu.edu>
2019-12-11 14:01:43 +0400

    could check the availability of multiple labels instead of one

--- fs_checklabel.m
+++ fs_checklabel.m
@@ -1,10 +1,29 @@
-function isAvailable = fs_checklabel(fn_label, subjCode)
-% This function checks if there is the label for this subject
+function [allAvail, isAvail] = fs_checklabel(labelList, subjCode)
+% This function checks if there is the label(s) for this subject
+%
+% Inputs:
+%    labelList        a list of label(s) (cell)
+%    subjCode         subject code in $SUBJECTS_DIR
+% Outputs:
+%    allAvail         if all the labels are available
+%    isAvail          the availability of all labels
 % 
 % Created by Haiyang Jin (28/11/2019)
 
+% convert to cell if the labelList is a string
+if ischar(labelList)
+    labelList = {labelList};
+end
+
+% FreeSurfer setup
 FS = fs_setup;
 
-label_file = fullfile(FS.subjects, subjCode, 'label', fn_label);
+labelPath = fullfile(FS.subjects, subjCode, 'label');
 
-isAvailable = exist(label_file, 'file');
+% the availability of each label
+isAvail = (cellfun(@(x) exist(fullfile(labelPath, x), 'file'), labelList));
+
+% availablility of all labels
+allAvail = all(isAvail);
+
+end
