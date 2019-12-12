d71075cb8904811b3da04d3f47a7be5e2d7716fa
Haiyang <haiyang.jin@nyu.edu>
2019-12-12 14:38:58 +0400

    read the run file
    could be the run filename with or without its path

---
+++ functional/fs_fun_readrun.m
@@ -0,0 +1,35 @@
+function [runNames, nRun] = fs_fun_readrun(run_fn, projStr, subjCode_bold)
+% This function loads run file (.txt) and output the list of run numbers
+% 
+% Created by Haiyang Jin (08/12/2019)
+%
+% Inputs:
+%    projStr     project information (e.g., fw_projectinfo)
+%    fn_run      filenames of the run file (*.txt) (without path)
+%    subjCode    subjCode in $SUBJECTS_DIR
+% Outputs:
+%    runNames    a vector of run names
+%    nRun        number of runs
+
+path = fileparts(run_fn);
+
+if isempty(path)
+    if isempty(projStr) || isempty(subjCode_bold)
+        error('Not enough inputs for fs_fun_readrun');
+    else
+        run_file = fullfile(projStr.fMRI, subjCode_bold, 'bold', run_fn);
+    end
+else
+    run_file = run_fn;
+end
+
+% read txt files
+runList = importdata(run_file); 
+
+% covnert to a cell of char
+runNames = arrayfun(@(x) sprintf('%03d', x), runList, 'UniformOutput', false);
+
+% number of runs
+nRun = numel(runList);
+
+end
