7a80a49d407bf86fce72ba9a88ac18fcbc4937db
Haiyang <haiyang.jin@nyu.edu>
2019-12-16 14:15:49 +0400

    now this function could save a cell of strings in a file
    The pervious version of function could only be used to save a string in the file

--- fs_createfile.m
+++ fs_createfile.m
@@ -1,17 +1,34 @@
-function fs_createfile(file, contents)
+function fs_createfile(file, contents4save)
 % This function creates new files for FreeSurfer (e.g, sessid, subjectname,
 % runfile).
 %
 % Inputs:
 %    file        the filename with path to be created
-%    contents    the contents to be saved (only 1 string)
+%    contents    the contents to be saved (could be a cell or a string)
 % Output:
 %    a new file is created.
 %
 % Created by Haiyang Jin (16/12/2019)
 
+if ischar(contents4save)
+    contents4save = {contents4save};
+end
+
+% size of the contents to be saved
+[nRow, nColu] = size(contents4save);
+
+% open a new file
 fid = fopen(file, 'w');
-fprintf(fid, contents);
+
+% create the array
+rowFormat = ['%s' repmat(' %s', 1, nColu-1)];
+
+allFormat = [rowFormat repmat(['\n' rowFormat], 1, nRow-1)];
+
+transcontent = contents4save';
+
+fprintf(fid, sprintf(allFormat, transcontent{:}));
+
 fclose(fid);
 
 end
