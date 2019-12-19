261c47c943e23fb60c87c5928a52975afade7cfd
Haiyang <haiyang.jin@nyu.edu>
2019-12-19 11:37:26 +0400

    add two more arguments: locSmooth, isfsavg
    locSmooth: the smooth of localizer (e.g., sm5)
    isfsavg: show the activation of the surface of fsaverage

--- functional/fs_fun_screenshot_label.m
+++ functional/fs_fun_screenshot_label.m
@@ -1,4 +1,4 @@
-function fs_fun_screenshot_label(projStr, labelList, output_path, whichOverlay)
+function fs_fun_screenshot_label(projStr, labelList, output_path, whichOverlay, locSmooth, isfsavg)
 % This function gets the screenshots of labels with overlays.
 %
 % Inputs:
@@ -15,8 +15,16 @@ function fs_fun_screenshot_label(projStr, labelList, output_path, whichOverlay)
 if nargin < 3 
     output_path = '';
 end
-if nargin < 4 
+if nargin < 4 || isempty(whichOverlay)
     whichOverlay = 1; % show the overlay of the first label by default
+end
+if nargin < 5 || isempty(locSmooth)
+    locSmooth = '';
+elseif ~strcmp(locSmooth(1), '_')
+    locSmooth = ['_' locSmooth];
+end
+if nargin < 6 || isempty(isfsavg)
+    isfsavg = '';
 end
 
 % number of labels
@@ -61,7 +69,7 @@ for iLabel = 1:nLabels
         waitbar(progress, f_single, wait_msg);
         
         % other information for screenshots
-        analysis = sprintf('loc%s.%s', boldext, hemi); % analysis name
+        analysis = sprintf('loc%s%s.%s', locSmooth, boldext, hemi); % analysis name
         file_overlay = fullfile(projStr.funcPath, thisBoldSubj, 'bold',...
             analysis, theContrast, 'sig.nii.gz'); % the overlay file
         
@@ -69,11 +77,12 @@ for iLabel = 1:nLabels
         if ~whichOverlay
             file_overlay = '';
         elseif ~exist(file_overlay, 'file')
+            warning('Cannot find the overlay file: %s', file_overlay);
             continue
         end
             
         % create the screenshot
-        fs_screenshot_label(subjCode, theLabel, output_path, file_overlay);
+        fs_screenshot_label(subjCode, theLabel, output_path, file_overlay, isfsavg);
 
     end
     
