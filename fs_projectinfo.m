4c6e40f96401e67e6244fb5f80c7ba71aec5455c
Haiyang <hjin317@aucklanduni.ac.nz>
2020-01-16 23:58:49 +0400

    rename from fs_setup to fs_projectinfo
    gather FreeSurfer subjects information

---
+++ fs_projectinfo.m
@@ -0,0 +1,35 @@
+function FS = fs_projectinfo(subjectsPath)
+% This function set up and gather some information from FreeSurfer.
+%
+% Input:
+%    subjectsPath   path to subjects/ folder in FreeSurfer
+%
+% Output:
+%    FS             a struct contains FreeSurfer information
+%
+% Created and updated by Haiyang Jin (16-Jan-2020)
+
+% get the enviorment variables on FreeSurfer
+FS.homedir = getenv('FREESURFER_HOME');
+
+% Default path to FreeSurfer
+if nargin < 1 
+    subjectsPath = fullfile(FS.homedir, 'subjects');
+end
+
+% set the subjects folder
+setenv('SUBJECTS_DIR', subjectsPath);
+FS.subjects = subjectsPath;
+
+% hemisphere information
+FS.hemis = {'lh', 'rh'};
+FS.nHemi = numel(FS.hemis);
+
+% subject code information
+subjdir = dir(FS.subjects);
+subjdir = subjdir([subjdir.isdir]);  % only keep folders
+FS.subjdir = subjdir(~ismember({subjdir.name}, {'.', '..'})); % remove . and ..
+FS.subjList = {FS.subjdir.name};
+FS.nSubj = numel(FS.subjList);
+
+end
