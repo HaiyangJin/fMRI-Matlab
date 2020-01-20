a0f45c3f353b00a7e887d40027afb90c807a8555
Haiyang <hjin317@aucklanduni.ac.nz>
2020-01-20 13:50:00 +0400

    set values to output variables if the run file is not available

--- functional/fs_fun_readrun.m
+++ functional/fs_fun_readrun.m
@@ -2,7 +2,7 @@ function [runNames, nRun] = fs_fun_readrun(runFn, project, sessCode)
 % This function loads run file (.txt) and output the list of run numbers
 %
 % Inputs:
-%    runFn      filenames of the run file (*.txt) (without path)
+%    runFn      filenames of the run file (*.txt) (with or without path)
 %    project    project information (obtained from fs_fun_projectinfo)
 %    sessCode    session code for functional data (functional subject code)
 % Outputs:
@@ -15,12 +15,21 @@ path = fileparts(runFn);
 
 if isempty(path)
     if isempty(project) || isempty(sessCode)
-        error('Not enough inputs for fs_fun_readrun');
+        error('Not enough inputs for fs_fun_readrun.');
     else
         runFile = fullfile(project.funcPath, sessCode, 'bold', runFn);
     end
 else
     runFile = runFn;
+end
+
+% return if the file is not available
+if ~exist(runFile, 'file')
+    runNames = {''};
+    nRun = 0;
+    [path, fn, ext] = fileparts(runFile);
+    warning('Cannot find %s at %s.', [fn, ext], path); 
+    return;
 end
 
 % read txt files
