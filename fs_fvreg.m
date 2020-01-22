608125805ff8ccadf65e5c8ad064129258839ef1
Haiyang <hjin317@aucklanduni.ac.nz>
2020-01-22 16:53:57 +0400

    new function to check co-registration before or after bbregistration

---
+++ fs_fvreg.m
@@ -0,0 +1,40 @@
+function fs_fvreg(project, sessCode, loadReg, runFolder)
+% This function displays the co-registration between structure and
+% functional data in FreeView.
+%
+% Inputs:
+%     project           project information (created by fs_fun_projectinfo)
+%     sessCode          <string> session code in functional folder 
+%     loadReg           <logical> 1: load the register.lta in the folder;
+%                       0: do not load the file. (register.lta is generated
+%                       by fs_bbregister.m which performs the automatic
+%                       registration.)
+%     runFolder         <string> the run folder names
+%
+% Output:
+%     display the registration (before or after bbregister)
+%
+% Created by Haiyang Jin (22-Jan-2020)
+
+if nargin < 3 || isempty(loadReg)
+    loadReg = 0;  % don't load the register.lta by default
+end
+
+if nargin < 4 || isempty(runFolder)
+    % show the first run by default
+    locList = fs_fun_readrun('run_loc.txt', project, sessCode);
+    mainList = fs_fun_readrun('run_main.txt', project, sessCode);
+    runList = [locList; mainList];
+    runFolder = runList{1};
+end
+
+% the template filename
+tempFile = fullfile(project.funcPath, sessCode, 'bold', runFolder, 'template.nii.gz');
+
+% subject code in SUBJECTS_DIR
+subjCode = fs_subjcode(sessCode, project.funcPath);
+
+% display the template.nii.gz with ?h.white and ?h.pial
+fs_fvvol(tempFile, subjCode, project.structPath, '', loadReg);
+
+end
