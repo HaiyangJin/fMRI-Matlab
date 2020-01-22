e826855f0d2cf4296dfb42aaa2b79926a871b18e
Haiyang <hjin317@aucklanduni.ac.nz>
2020-01-22 14:56:06 +0400

    the filenames could be filenames with or without path now

--- fs_hemi_multi.m
+++ fs_hemi_multi.m
@@ -1,4 +1,4 @@
-function [hemi, nHemi] = fs_hemi_multi(filenames, forceString)
+function [hemi, nHemi] = fs_hemi_multi(files, forceString)
 % This function determine the hemispheres based on the filenames (if 'lh'
 % or 'rh' is included in the filename).
 %
@@ -15,13 +15,18 @@ function [hemi, nHemi] = fs_hemi_multi(filenames, forceString)
 % Created by Haiyang Jin (1-Dec-2019)
 
 % convert the filenames to a cell if it is a string
-if ischar(filenames)
-    filenames = {filenames};
+if ischar(files)
+    files = {files};
 end
 
 if nargin < 2 || isempty(forceString)
     forceString = 1;
 end
+
+% remove path if it is included in filenames
+[~, nameCell, extCell] = cellfun(@fileparts, files, 'uni', false);
+% get filenames for all files
+filenames = cellfun(@(x, y) [x y], nameCell, extCell, 'uni', false);
 
 % hemi for each file
 hemis = cellfun(@fs_hemi, filenames, 'UniformOutput', false);
@@ -33,7 +38,9 @@ if nHemi == 1 && forceString
     hemi = hemis{1};
 else
     hemi = hemis;
-    warning('These files are for both hemispheres.');
+    if nHemi == 2
+        warning('These files are for both hemispheres.');
+    end
 end
 
 end
