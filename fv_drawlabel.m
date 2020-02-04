6a7ad4f9c9464e0b7b85ff33abe739c653d3d387
Haiyang <hjin317@aucklanduni.ac.nz>
2020-02-04 10:24:00 +0400

    make sure the sigFile is available

--- fv_drawlabel.m
+++ fv_drawlabel.m
@@ -1,8 +1,8 @@
-function fv_drawlabel(subjCode, hemi, fileSig, labelname)
+function fv_drawlabel(subjCode, hemi, sigFile, labelname)
 % fv_drawlabel(subjCode, hemi, fileSig, labelname)
 %
-% This function uses "tksurfer" in FreeSurfer to draw label 
-% 
+% This function uses "tksurfer" in FreeSurfer to draw label
+%
 % Inputs:
 %    subjCode         subject code in $SUBJECTS_DIR
 %    hemi             which hemispheres (must be 'lh' or 'rh')
@@ -17,16 +17,25 @@ function fv_drawlabel(subjCode, hemi, fileSig, labelname)
 
 subjPath = getenv('SUBJECTS_DIR');
 
+% make sure the sigFile exists
+if ~exist(sigFile, 'file')
+    error('Cannot find the file: %s.', sigFile);
+end
+
 % create FreeSurfer command and run it
 fscmd = sprintf('tksurfer %s %s inflated -aparc -overlay %s',...
-    subjCode, hemi, fileSig);
+    subjCode, hemi, sigFile);
 system(fscmd);
 
 %%%%%%%%%%%%%%%% Manual working in FreeSurfer %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % IMPORTANT: Please make sure you started X11
-% 1. click any vertex in the region
-% 2. fill it with "similar" vertices
-% 3. Save that area as a label file
+% 1. click any vertex in the region;
+% 2. fill it with "similar" vertices;
+%    1.Custom Fill;
+%    2.make sure "Up to and including paths" and "up to funcitonal values
+%      below threhold" are selected;
+%    3.click "Fill".
+% 3. Save that area as a label file;
 % NOTE: please name the label as label.label in the folder
 % ($SUBJECTS_DIR/subjCode/label.label) Basically, you only need to delect
 % the "/" in the default folder or filename
