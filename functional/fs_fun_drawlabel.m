0cc662feb9f6ef98989b195fa4346784e1727517
Haiyang <hjin317@aucklanduni.ac.nz>
2020-02-04 10:25:00 +0400

    add another argument (in) for the analysis names

--- functional/fs_fun_drawlabel.m
+++ functional/fs_fun_drawlabel.m
@@ -1,32 +1,37 @@
-function fs_fun_drawlabel(project, contrastName, siglevel, extraLabelInfo)
+function fs_fun_drawlabel(project, analysisName, contrastName, siglevel, extraLabelInfo)
+% fs_fun_drawlabel(project, analysisName, contrastName, siglevel, extraLabelInfo)
 % This function use FreeSurfer ("tksurfer") to draw labels.
 %
 % Inputs: 
-%    project           matlab structure for the project (obtained from fs_fun_projectinfo)
-%                      This is specific for each project.
-%    contrast_name     contrast name used glm
+%    project           <structure> matlab structure for the project (obtained from 
+%                      fs_fun_projectinfo)
+%    analysisName      <string> or <a cell of strings> the names of the
+%                      analysis (i.e., the names of the analysis folders)
+%    contrast_name     contrast name used glm (i.e., the names of contrast
+%                      folders)
 %    siglevel          significance level (default is f13 (.05))
 %    extraLabelInfo    extra label information added to the label name
 % Output:
-%    a label saved in the label folder within $SUBJECTS_DIR
+%    a label saved in the label/ folder within $SUBJECTS_DIR
 %
 % Created by Haiyang Jin (10/12/2019)
 
-
-if nargin < 3 || isempty(siglevel)
+if nargin < 4 || isempty(siglevel)
     siglevel = 'f13';
 end
-if nargin < 4 || isempty(extraLabelInfo)
+if nargin < 5 || isempty(extraLabelInfo)
     extraLabelInfo = '';
 elseif ~strcmp(extraLabelInfo(end), '.')
     extraLabelInfo = [extraLabelInfo, '.'];
 end
 
+% convert analysisName to cell if it is string
+if ischar(analysisName); analysisName = {analysisName}; end
+nAnalysis = numel(analysisName);
+
 % obtian the information about this bold type
-boldext = project.boldext;
 sessList = project.sessList;
 nSess = project.nSess;
-hemis = project.hemis;
 
 %% Draw labels for all participants for both hemispheres
 
@@ -35,18 +40,19 @@ for iSess = 1:nSess
     thisSess = sessList{iSess};
     subjCode = fs_subjcode(thisSess, project.funcPath);
     
-    for iHemi = 1:project.nHemi
+    for iAna = 1:nAnalysis
         
-        hemi = hemis{iHemi};
+        thisAna = analysisName{iAna};
+        hemi = fs_hemi(thisAna);
         
         sigFile = fullfile(project.funcPath, thisSess, 'bold',...
-            ['loc' boldext '.' hemi], contrastName, 'sig.nii.gz');
+            thisAna, contrastName, 'sig.nii.gz');
         
         labelName = sprintf('roi.%s.%s.%s.%slabel', ...
             hemi, siglevel, contrastName, extraLabelInfo);
         
         % draw labels manually with FreeSurfer
-        fs_drawlabel(subjCode, hemi, sigFile, labelName);
+        fv_drawlabel(subjCode, hemi, sigFile, labelName);
         
     end
     
