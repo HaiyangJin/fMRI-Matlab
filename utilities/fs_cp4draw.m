476483771822f6801604b465ec675017b84d7aef
Haiyang <hjin317@aucklanduni.ac.nz>
2020-02-23 18:38:24 +0400

    copied more files for running tksurfer-sess in FreeSurfer 6
    tksurfer-sess in FreeSurfer 6 will use freeview, which may not help

--- utilities/fs_cp4draw.m
+++ utilities/fs_cp4draw.m
@@ -18,6 +18,7 @@ function fs_cp4draw(project, targetPath, analysisList, contrastList)
 %           surf/?h.orig
 %           surf/?h.curv
 %           surf/?h.white
+%           surf/?h.pial
 %           label/?h.aparc.annot
 %           mri/orig.mgz
 %           mri/transforms/talairach.xfm
@@ -26,6 +27,8 @@ function fs_cp4draw(project, targetPath, analysisList, contrastList)
 %
 %     funcPath/sessCode/subjectname
 %
+%     funcPath/analysisname
+%
 % Created by Haiyang Jin (10-Feb-2020)
 
 structPath = project.structPath;
@@ -33,7 +36,11 @@ structPath = project.structPath;
 funcPath = project.funcPath;
 [~, funcFolder] = fileparts(funcPath);
 
-surfFiles = {'*h.inflated', '*h.orig', '*h.curv', '*h.white'};
+surfFiles = {'*h.inflated', '*h.orig', '*h.curv', '*h.white', '*h.pial'};
+
+% copy the analysis folders in funcPath
+cellfun(@fs_copyfile, fullfile(funcPath, analysisList), ...
+    fullfile(targetPath, funcFolder, analysisList));
 
 for iSess = 1: project.nSess
     
@@ -43,10 +50,15 @@ for iSess = 1: project.nSess
     tempBold = fullfile(thisSess, 'bold');
     
     [tempAna, tempCon] = ndgrid(analysisList, contrastList);
-    
+   
+    % copy all the contrast folders
     cellfun(@(x, y) fs_copyfile(fullfile(funcPath, tempBold, x, y), ...
         fullfile(targetPath, funcFolder, tempBold, x, y)), tempAna, tempCon, ...
         'uni', false);
+    
+    % subjectname
+    fs_copyfile(fullfile(funcPath, thisSess, 'subjectname'), ...
+        fullfile(targetPath, funcFolder, thisSess));
     
     % structural data
     thisSubj = fs_subjcode(thisSess, funcPath);
@@ -63,10 +75,6 @@ for iSess = 1: project.nSess
         fullfile(targetPath, structFolder, tempOrig));
     fs_copyfile(fullfile(structPath, tempOrig, 'transforms', 'talairach.xfm'), ...
         fullfile(targetPath, structFolder, tempOrig, 'transforms'));
-    
-    % subjectname
-    fs_copyfile(fullfile(funcPath, thisSess, 'subjectname'), ...
-        fullfile(targetPath, funcFolder, thisSess));
     
 end
 
