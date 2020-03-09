efda32651d99219774c4b909723acfa8d8f52ce5
Haiyang <hjin317@aucklanduni.ac.nz>
2020-03-09 15:23:13 +0400

    rename fs_cosmo_univariate as fs_ds2uni

---
+++ utilities/fs_ds2uni.m
@@ -0,0 +1,32 @@
+function uniTable = fs_ds2uni(ds, condInfo)
+% This function convert the ds from CoSMoMVPA to a table for univariate
+% analysis.
+%
+% Inputs:
+%     ds                <structure> data set obtained from CoSMoMVPA.
+%     condInfo          <structure> condition information.
+%
+% Output:
+%    uniTable           <table> data table for univariate analysis.
+%
+% Created by Haiyang Jin (18-Nov-2019)
+
+%% Convert dataset structure to table
+dsTable = table;
+
+dsTable.Resp = mean(ds.samples, 2);
+dsTable.Conditions = ds.sa.labels;
+dsTable.Target = ds.sa.targets;
+
+%% Add condition information if avaiable
+if ~isempty(condInfo)
+    % add condiInfo if it is available
+    nRowUni = size(ds.samples, 1);
+    uniTable = [repmat(condInfo, nRowUni, 1), dsTable];
+    
+else
+    % do not use condition information
+    uniTable = dsTable;
+end
+
+end
