fed3b4049cea8d3e566bb2dc466b82d4cd472c9c
Haiyang <hjin317@aucklanduni.ac.nz>
2020-04-06 09:49:44 +0400

    new function to load FreeSurfer nifti files
    it is based on load_nifti.m in FreeSurfer Matlab functions.

---
+++ utilities/fs_readnifti.m
@@ -0,0 +1,42 @@
+function out = fs_readnifti(niiFilename, dataOnly)
+% out = fs_loadnifti(niiFilename, dataOnly)
+%
+% This function load nifti file created by FreeSurfer. It is built based on
+% FreeSurfer Matlab function (load_nifti).
+% If need to rearrange the dimention of the data, use shiftdim(data,3).
+%
+% Inputs:
+%    niiFilename       <string> the full filename of the nifti file.
+%    dataOnly          <numeric> 1: only output the data but not the
+%                       headers [the output will be a numeric array];
+%                       0: output all the information including the deaders
+%                       [the output will be a structure]; 2: only output
+%                       the headers [the output will be a structure].
+%
+% Output:
+%    out               <numeric array> when dataOnly is 1;
+%                      <structure> when dataOnly is 0 or 2.
+%
+% Dependency:
+%    FreeSurfer Matlab functions.
+%
+% Created by Haiyang Jin (6-April-2020)
+
+if nargin < 2 || isempty(dataOnly)
+    dataOnly = 1;
+end
+
+switch dataOnly
+    case 0 
+        % output all infromation
+        out = load_nifti(niiFilename);
+    case 1 
+        % only output the data
+        outtemp = load_nifti(niiFilename);
+        out = squeeze(outtemp.vol); % remove dimensions of length 1 
+    case 2
+        % only output headers
+        out = load_nifti(niiFilename, 1);
+end
+
+end
