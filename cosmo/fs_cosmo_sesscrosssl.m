8a491089d1dd37d9ebae2837428f9e42d646a674
Haiyang <hjin317@aucklanduni.ac.nz>
2020-04-08 15:28:09 +0400

    add new argument 'template'

--- cosmo/fs_cosmo_sesscrosssl.m
+++ cosmo/fs_cosmo_sesscrosssl.m
@@ -1,32 +1,36 @@
-function fs_cosmo_sesscrosssl(sessList, classPairs, surfType, combineHemi, ...
-    classifier, funcPath)
-% fs_cosmo_sesscrosssl(sessList, classPairs, surfType, combineHemi, ...
-%    classifier, funcPath)
+function fs_cosmo_sesscrosssl(sessList, classPairs, surfType, template, ...
+    combineHemi, classifier, funcPath)
+% fs_cosmo_sesscrosssl(sessList, classPairs, [surfType = 'sphere', template = 'fsaverage', ...
+%    combineHemi = 0, classifier = '', funcPath])
 %
 % This function does the searchlight analyses for the whole project
 % with CoSMoMVPA. Data were analyzed with FreeSurfer.
 %
 % Inputs:
-%     sessList           <string> or <cell of strings> session code 
-%                         (functional subject folder).
-%     classPairs         <cell of strings> a PxQ (usually is 2) cell matrix 
-%                         for the pairs to be classified. Each row is one 
-%                         classfication pair. 
-%     surfType           <string> the coordinate file for vertices (
-%                         ('sphere', 'inflated', 'white', 'pial').
-%     combineHemi        <logical> if the data of two hemispheres will be 
-%                         combined (default is no) [0: run searchlight for 
-%                         the two hemnispheres separately; 1: run searchlight
-%                         anlaysis for the whole brain together; 3: run
-%                         analysis for both 0 and 1.
-%     classifier         <numeric> or <strings> or <cells> the classifiers 
-%                         to be used (only 1).
-%     funcPath         <string> the full path to the functional folder.
+%    sessList           <string> or <cell of strings> session code 
+%                        (functional subject folder).
+%    classPairs         <cell of strings> a PxQ (usually is 2) cell matrix 
+%                        for the pairs to be classified. Each row is one 
+%                        classfication pair. 
+%    surfType           <string> the coordinate file for vertices (
+%                        ('sphere', 'inflated', 'white', 'pial').
+%    template           <string> 'fsaverage' or 'self'. fsaverage is default.
+%    combineHemi        <logical> if the data of two hemispheres will be 
+%                        combined (default is no) [0: run searchlight for 
+%                        the two hemnispheres separately; 1: run searchlight
+%                        anlaysis for the whole brain together; 3: run
+%                        analysis for both 0 and 1.
+%    classifier         <numeric> or <strings> or <cells> the classifiers 
+%                        to be used (only 1).
+%    funcPath           <string> the full path to the functional folder.
 %
 % Output:
-%     For each hemispheres, the results will be saved as a *.mgz file saved 
-%     at the subject label folder ($SUBJECTS_DIR/subjCode/surf)
-%     For the whole brain, the results will be saved as *.gii
+%    For each hemispheres, the results will be saved as a *.mgz file saved 
+%    at the subject label folder ($SUBJECTS_DIR/subjCode/surf)
+%    For the whole brain, the results will be saved as *.gii
+%
+% Dependency:
+%    CoSMoMVPA
 %
 % Created by Haiyang Jin (24-Nov-2019)
 
@@ -36,15 +40,19 @@ if nargin < 3 || isempty(surfType)
     surfType = 'sphere';
 end
 
-if nargin < 4 || isempty(combineHemi)
+if nargin < 4 || isempty(template)
+    template = '';
+end
+
+if nargin < 5 || isempty(combineHemi)
     combineHemi = 0;
 end
 
-if nargin < 5
+if nargin < 6
     classifier = '';
 end
 
-if nargin < 6 || isempty(funcPath)
+if nargin < 7 || isempty(funcPath)
     funcPath = getenv('FUNCTIONALS_DIR');
 end
 
@@ -66,18 +74,18 @@ for iSess = 1:nSess
     
     % waitbar
     progress = iSess / nSess * 1/2;
-    progressMsg = sprintf('Loading data for %s.   \n%0.2f%% finished...', ...
-        strrep(subjCode, '_', '\_'), progress*100);
+    progressMsg = sprintf('Loading data for %s (%s).   \n%0.2f%% finished...', ...
+        strrep(subjCode, '_', '\_'), template, progress*100);
     waitbar(progress, waitHandle, progressMsg);
     
+    % decide the target subject for vertex coordinates based on template
+    trgSubj = fs_trgsubj(subjCode, template);
     % load vertex and face coordinates
-    [vtxCell, faceCell] = fs_cosmo_surfcoor(subjCode, surfType, combineHemi);
-    
+    [vtxCell, faceCell] = fs_cosmo_surfcoor(trgSubj, surfType, combineHemi);
     
     %% Load functional data (beta.nii.gz)
-    % load the beta.nii.gz for both hemispheres separately [only use 
-    % fsaverage as the template]
-    dsSurfCell = cellfun(@(x) fs_cosmo_subjds(thisSess, x, 'fsaverage', ...
+    % load the beta.nii.gz for both hemispheres separately 
+    dsSurfCell = cellfun(@(x) fs_cosmo_subjds(thisSess, x, template, ...
         funcPath, 'main', 0, 1), hemis, 'uni', false);
     
     % combine the surface data for the whole brain if needed
@@ -95,7 +103,6 @@ for iSess = 1:nSess
             runSearchlight = 1:3;
         end
     end
-    
     
     %% conduct searchlight for two hemisphere seprately (and the whole brain)
     for iSL = runSearchlight  % SL = searchlight
@@ -120,12 +127,12 @@ for iSess = 1:nSess
         featureCount = 200;
         
         % run search light analysis
-        fs_cosmo_crosssl(ds_this, surfDef, featureCount, classPairs, ...
-            subjCode, hemiInfo, classifier);
+        fs_cosmo_crosssl(ds_this, classPairs, surfDef, featureCount, ...
+            subjCode, hemiInfo, template, classifier);
         
-    end
+    end  % iSL
     
-end
+end  % iSess
 
 % close the waitbar 
 close(waitHandle); 
