7d58073c0c4930a64d3ae1afe7e75f442d145747
Haiyang <hjin317@aucklanduni.ac.nz>
2020-04-08 11:16:26 +0400

    update sessCode as sessList
    can run for multiple sessions now

--- freesurfer/fs_projsess.m
+++ freesurfer/fs_projsess.m
@@ -1,6 +1,7 @@
-function fscmd = fs_projsess(sessCode, projFile, template, sm, runList, hemi, funcPath)
-% fscmd = fs_projsess(sessCode, projFile, template, sm, runList, hemi, funcPath)
-% 
+function fscmd = fs_projsess(sessList, projFile, template, smooth, runList, hemi, funcPath)
+% fscmd = fs_projsess(sessList, projFile, [template = 'fsaverage', smooth = 0,
+%                     runList = [allruns], hemi = {'lh', 'rh'}, funcPath])
+%
 % This function projects the preprocessed functional (voxelwise) data for
 % that whole session to fsaverage or self surface. Then FWHM smoothing is
 % applied if needed. The additional output (compared with fs_projfunc.m and
@@ -8,12 +9,12 @@ function fscmd = fs_projsess(sessCode, projFile, template, sm, runList, hemi, fu
 %     brain.[template].?h.pr.nii.gz
 %
 % Inputs:
-%     sessCode         <string> session code in funcPath.
+%     sessList         <string> session code (list) in funcPath.
 %     projFile         <string> the name of the to-be-projected file
 %                       (i.e., the preprocessed functional data).
 %                       [do not need to include '.nii.gz'.]
 %     template         <string> 'fsaverage' or 'self'. fsaverage is the default.
-%     sm               <integer> smoothing with FWHM.
+%     smooth           <integer> smoothing with FWHM.
 %     runList          <cell of strings> a list of run folder names, OR
 %                      <string> the name of the run file. All runs are
 %                      processed by default.
@@ -36,8 +37,8 @@ elseif ~ismember(template, {'fsaverage', 'self'})
     error('The template has to be ''fsaverage'' or ''self'' (not ''%s'').', template);
 end
 
-if nargin < 4 || isempty(sm)
-    sm = 0;
+if nargin < 4 || isempty(smooth)
+    smooth = 0;
 end
 
 if nargin < 6 || isempty(hemi)
@@ -48,42 +49,55 @@ if nargin < 7 || isempty(funcPath)
     funcPath = getenv('FUNCTIONALS_DIR');
 end
 
-% the bold path
-boldPath = fullfile(funcPath, sessCode, 'bold');
-% the list of all runs
-runListAll = fs_runlist(boldPath);
-
-if nargin < 5 || isempty(runList)
-    % run the analysis for all the runs
-    runList = runListAll;
-elseif ischar(runList)
-    % get the list of run folder names
-    runList = fs_readtext(fullfile(boldPath, runList));
+if ischar(sessList)
+    sessList = {sessList};
 end
-
-% all the combinations of hemispheres and runs
-[hemis, runs] = ndgrid(hemi, runList);
+nSess = numel(sessList);
 
 % empty cell for saving FreeSurfer commands
-fscmd = cell(3, 1);
+fscmd = cell(3, nSess);
 
-%% Project masks for all runs
-fscmd1 = cellfun(@(x, y) fs_projmask(sessCode, x, template, y, funcPath), ...
-    runs(:), hemis(:), 'uni', false);
-fscmd{1, 1} = vertcat(fscmd1{:});
+% project data for each session separately
+for iSess = 1:nSess
+    
+    % this session code
+    thisSess = sessList{iSess};
+    
+    % the bold path
+    boldPath = fullfile(funcPath, thisSess, 'bold');
+    % the list of all runs
+    runListAll = fs_runlist(boldPath);
+    
+    if nargin < 5 || isempty(runList)
+        % run the analysis for all the runs
+        runList = runListAll;
+    elseif ischar(runList)
+        % get the list of run folder names
+        runList = fs_readtext(fullfile(boldPath, runList));
+    end
+    
+    % all the combinations of hemispheres and runs
+    [hemis, runs] = ndgrid(hemi, runList);
+    
+    %% Project masks for all runs
+    fscmd1 = cellfun(@(x, y) fs_projmask(thisSess, x, template, y, funcPath), ...
+        runs(:), hemis(:), 'uni', false);
+    fscmd{1, iSess} = vertcat(fscmd1{:});
+    
+    %% Project functional data for all runs
+    fscmd2 = cellfun(@(x, y) fs_projfunc(thisSess, projFile, x, template, y, ...
+        smooth, funcPath), runs(:), hemis(:), 'uni', false);
+    fscmd{2, iSess} = vertcat(fscmd2{:});
+    
+    %% Create the mean masks of all runs if all runs are projected
+    if all(ismember(runList, runListAll))
+        fscmd3 = fs_sessmeanmask(thisSess, template, funcPath);
+        fscmd{3, iSess} = fscmd3;
+    end
+    
+    %% save the FreeSurfer commands as one column
+    fscmd = vertcat(fscmd{:});
+    
+end  % iSess
 
-%% Project functional data for all runs
-fscmd2 = cellfun(@(x, y) fs_projfunc(sessCode, projFile, x, template, y, ...
-    sm, funcPath), runs(:), hemis(:), 'uni', false);
-fscmd{2, 1} = vertcat(fscmd2{:});
-
-%% Create the mean masks of all runs if all runs are projected 
-if all(ismember(runList, runListAll))
-    fscmd3 = fs_sessmeanmask(sessCode, template, funcPath);
-    fscmd{3, 1} = fscmd3;
-end
-
-%% save the FreeSurfer commands as one column
-fscmd = vertcat(fscmd{:});
-
-end
+end  % function ends
