13ca4496c3876d9c56b924e9bf3a2b0fa99537fd
Haiyang <hjin317@aucklanduni.ac.nz>
2020-04-09 11:16:31 +0400

    new function to load ?h.cortex.label to mask vertices

---
+++ utilities/fs_cortexmask.m
@@ -0,0 +1,46 @@
+function [corMask, nCorMask] = fs_cortexmask(subjCode, hemi, struPath)
+% [corMask, nCorMask] = fs_cortexmask([subjCode = 'fsaverage', hemi = 'lh', struPath])
+%
+% This function load ?h.cortex.label and output the vertex indices as the
+% mask for surfaces. 
+%
+% Inputs:
+%    subjCode         <string> subject code in $SUBJECTS_DIR.
+%    hemi             <string> 'lh' or 'rh'.
+%    struPath         <string> full path to the subject folder
+%                      ($SUBJECTS_DIR).
+%
+% Outputs:
+%    corMask          <numeric vector> a vector of the vertex indices.
+%    nCorMask         <integer> number of vertices in the mask. 
+%
+% Created by Haiyang Jin
+
+if nargin < 1 || isempty(subjCode)
+    subjCode = 'fsaverage';
+end
+
+if nargin < 2 || isempty(hemi)
+    hemi = 'lh';
+end
+% sanity check
+if ~ismember(hemi, {'lh', 'rh'})
+    error('''hemi'' has to be ''lh'' or ''rh'' (not %s).', hemi);
+end
+
+if nargin < 3 || isempty(struPath)
+    struPath = getenv('SUBJECTS_DIR');
+end
+
+% path and filename of the label file
+labelFn = sprintf('%s.cortex.label', hemi);
+labelPath = fullfile(struPath, subjCode, 'label');
+
+% load the label information
+labelData = fs_readlabel(fullfile(labelPath, labelFn));
+
+% outputs
+corMask = labelData(:, 1);
+nCorMask = numel(corMask);
+
+end
