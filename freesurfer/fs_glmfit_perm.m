627562b3c2d1321c8d25d1ece55815cd848b51c4
Haiyang <hjin317@aucklanduni.ac.nz>
2020-04-14 16:01:30 +0400

    change njobs to ncores and update for optional inputs

--- freesurfer/fs_glmfit_perm.m
+++ freesurfer/fs_glmfit_perm.m
@@ -1,5 +1,5 @@
-function fscmd = fs_glmfit_perm(glmdir, njobs, nSim, vwthrehold, sign, cwp, spaces, overwrite)
-% fscmd = fs_glmfit_perm([glmdir = {'.'}, njobs = 1, nSim = 5000, ...
+function fscmd = fs_glmfit_perm(glmdir, ncores, nSim, vwthrehold, sign, cwp, spaces, overwrite)
+% fscmd = fs_glmfit_perm([glmdir = {'.'}, cores = 1, nSim = 5000, ...
 %    vwthrehold = 3, sign = 'abs', cwp = 0.05, spaces = 2, overwrite = 1])
 %
 % This function runs permutation to calculate the "p-values" (via
@@ -9,7 +9,7 @@ function fscmd = fs_glmfit_perm(glmdir, njobs, nSim, vwthrehold, sign, cwp, spac
 %    glmdir        <string> or <cell of strings> path to the glmdir
 %                   folders. This can be obtained from fs_glmfit_osgm.m.
 %                   [glmdir is the current folder by default.]
-%    njobs         <integer> number of jobs to be used for the simulation
+%    ncores        <integer> number of jobs to be used for the simulation
 %                   (i.e., the simulation [permutation] are divided into
 %                   njobs). [Default is 1].
 %    nSim          <integer> number of simulations. [Default is 5000].
@@ -34,53 +34,42 @@ function fscmd = fs_glmfit_perm(glmdir, njobs, nSim, vwthrehold, sign, cwp, spac
 %
 % Created by Haiyang Jin (12-Apr-2020)
 
-if nargin < 1 || isempty(glmdir)
+if ~exist('glmdir', 'var') || isempty(glmdir)
     glmdir = {'.'};
 elseif ischar(glmdir)
     glmdir = {glmdir};
 end
-
-if nargin < 2 || isempty(njobs)
-    njobs = 1;
+if ~exist('ncores', 'var') || isempty(ncores)
+    ncores = 1;
 end
-
-if nargin < 3 || isempty(nSim)
+if ~exist('nSim', 'var') || isempty(nSim)
     nSim = 5000; % nsim vwthreshold sign
 end
-
-if nargin < 4 || isempty(vwthrehold)
+if ~exist('vwthrehold', 'var') || isempty(vwthrehold)
     vwthrehold = 3; % voxel-wise (clutering form) threshold
 end
-
-if nargin < 5 || isempty(sign)
+if ~exist('sign', 'var') || isempty(sign)
     sign = 'abs';
 end
-
-if nargin < 6 || isempty(cwp)
+if ~exist('cwp', 'var') || isempty(cwp)
     cwp = .05;  % cluster-wise p-value threshold
 end
-
-if nargin < 7 || isempty(spaces)
+if ~exist('spaces', 'var') || isempty(spaces)
     spaces = 2;
 end
-
-if nargin < 8 || isempty(overwrite)
+if ~exist('overwrite', 'var') || isempty(overwrite)
     overwrite = 1;
 end
-
-if overwrite
-    ow = ' --overwrite';
-else
-    ow = '';
-end
+ow = {'', ' --overwrite', ''};
+owArg = ow{overwrite + 1};
 
 % parameters for permutation
-bg = sprintf(' --bg %d', njobs);
+bg = sprintf(' --bg %d', ncores);
 perm = sprintf('%d %d %s', nSim, vwthrehold, sign);
 
 % create FreeSurfer commands
 fscmd = cellfun(@(x) sprintf(['mri_glmfit-sim --glmdir %s --perm %s '...
-    ' --cwp %d  --%dspaces %s%s'], x, perm, cwp, spaces, bg, ow), ...
+    ' --cwp %d  --%dspaces %s%s'], x, perm, cwp, spaces, bg, owArg), ...
     glmdir, 'uni', false);
 
 if overwrite ~= 2
