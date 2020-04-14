1b9b3d11b84685c3822357da2f0323cd038e70fe
Haiyang <hjin317@aucklanduni.ac.nz>
2020-04-14 10:09:04 +0400

    do not run fscmd when overwrite is 2

--- freesurfer/fs_glmfit_perm.m
+++ freesurfer/fs_glmfit_perm.m
@@ -2,7 +2,7 @@ function fscmd = fs_glmfit_perm(glmdir, njobs, nSim, vwthrehold, sign, cwp, spac
 % fscmd = fs_glmfit_perm([glmdir = {'.'}, njobs = 1, nSim = 5000, ...
 %    vwthrehold = 3, sign = 'abs', cwp = 0.05, spaces = 2, overwrite = 1])
 %
-% This function runs permutation to calculate the "p-values" (via 
+% This function runs permutation to calculate the "p-values" (via
 % mri_glmfit-sim).
 %
 % Inputs:
@@ -13,23 +13,24 @@ function fscmd = fs_glmfit_perm(glmdir, njobs, nSim, vwthrehold, sign, cwp, spac
 %                   (i.e., the simulation [permutation] are divided into
 %                   njobs). [Default is 1].
 %    nSim          <integer> number of simulations. [Default is 5000].
-%    vwthrehold    <numeric> voxel[vertex]-wise (clutering form) threshold. 
+%    vwthrehold    <numeric> voxel[vertex]-wise (clutering form) threshold.
 %                   -log(p). [Default is 2 (i.e., p < .01)].
 %    sign          <string> the direction of the test. ['pos', 'neg',
 %                   'abs']. [Default is 'abs'].
 %    cwp           <numeric> cluster-wise p-value threshold. [Default is
 %                   0.05].
-%    spaces        <integer> 2 or 3. Additional Bonferroni correction 
+%    spaces        <integer> 2 or 3. Additional Bonferroni correction
 %                   across 2 spaces (eg, lh, rh) or 3 (eg, lh, rh, mni305).
 %                   [Default is 2].
 %    overwrite     <logical> 1: overwrite the permuation. 0: do not
-%                   overwrite the permutation run before.
+%                   overwrite the permutation run before. 2: do not run
+%                   FreeSurfer commands but only output fscmd.
 %
 % Output:
-%    fscmd         <cell of strings> The first column is FreeSurfer 
-%                   commands used in the current session. And the second 
-%                   column is whether the command successed. 
-%                   [0: successed; other numbers: failed.] 
+%    fscmd         <cell of strings> The first column is FreeSurfer
+%                   commands used in the current session. And the second
+%                   column is whether the command successed.
+%                   [0: successed; other numbers: failed.]
 %
 % Created by Haiyang Jin (12-Apr-2020)
 
@@ -48,15 +49,15 @@ if nargin < 3 || isempty(nSim)
 end
 
 if nargin < 4 || isempty(vwthrehold)
-    vwthrehold = 3; % voxel-wise (clutering form) threshold 
+    vwthrehold = 3; % voxel-wise (clutering form) threshold
 end
 
 if nargin < 5 || isempty(sign)
-    sign = 'abs';  
+    sign = 'abs';
 end
 
 if nargin < 6 || isempty(cwp)
-    cwp = .05;  % cluster-wise p-value threshold 
+    cwp = .05;  % cluster-wise p-value threshold
 end
 
 if nargin < 7 || isempty(spaces)
@@ -71,7 +72,7 @@ if overwrite
     ow = ' --overwrite';
 else
     ow = '';
-end    
+end
 
 % parameters for permutation
 bg = sprintf(' --bg %d', njobs);
@@ -80,12 +81,17 @@ perm = sprintf('%d %d %s', nSim, vwthrehold, sign);
 % create FreeSurfer commands
 fscmd = cellfun(@(x) sprintf(['mri_glmfit-sim --glmdir %s --perm %s '...
     ' --cwp %d  --%dspaces %s%s'], x, perm, cwp, spaces, bg, ow), ...
-    glmdir, 'uni', false); 
+    glmdir, 'uni', false);
 
-% run commands 
-isnotok = cellfun(@system, fscmd);
-if any(isnotok)
-    warning('Some FreeSurfer commands (mri_glmfit) failed.');
+if overwrite ~= 2
+    % run FreeSurfer commands
+    isnotok = cellfun(@system, fscmd);
+    if any(isnotok)
+        warning('Some FreeSurfer commands (mri_glmfit) failed.');
+    end
+else
+    % do not run fscmd
+    isnotok = zeros(size(fscmd));
 end
 
 % make the fscmd one column
