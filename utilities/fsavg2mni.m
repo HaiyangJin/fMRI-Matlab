c57f27314c1eb34d91d6530d7d610d112cea6592
Haiyang <hjin317@aucklanduni.ac.nz>
2020-04-15 17:36:18 +0400

    input points can be a P x 3 matrix now

--- utilities/fsavg2mni.m
+++ utilities/fsavg2mni.m
@@ -1,14 +1,21 @@
 function outpoints = fsavg2mni(inpoints)
+% outpoints = fsavg2mni(inpoints)
 % 
 % This function converts fsaverage (MNI305) to MNI152.
 % Parameters are obtained from FreeSurf website.
 % https://surfer.nmr.mgh.harvard.edu/fswiki/CoordinateSystems (check 8)
 %
+% Input:
+%    inpoints    <numeric array> a P x 3 matrix. Each row is one point and
+%                 the three columns are [R A S].
+% Output:
+%    outpoints   <numeric array> a P x 3 matrix in MNI152 space.
+%
 % Usage: outpoints = fsavg2mni([R A S]);
 %
-% Created by Haiyang Jin (13/11/2019)
+% Created by Haiyang Jin (13-Nov-2019)
 
-inRAS = [inpoints 1]';
+inRAS = horzcat(inpoints, ones(size(inpoints, 1), 1))';
 
 % matrix obtained from FreeSurf website
 matrix = [0.9975   -0.0073    0.0176   -0.0429;
