b8c65e1f9b75acae866270a1f84971772aaabd01
Haiyang <hjin317@aucklanduni.ac.nz>
2020-04-22 09:53:29 +0400

    remove the in-argument of 'hemi' and add defaults for in-arguments

--- utilities/fs_readsurf.m
+++ utilities/fs_readsurf.m
@@ -1,15 +1,16 @@
-function [coords, faces] = fs_readsurf(surfType, hemi, subjCode, struPath)
-% [coords, faces] = fs_readsurf(surfType, hemi, subjCode, struPath)
+function [coords, faces] = fs_readsurf(surfFn, subjCode, struPath)
+% [coords, faces] = fs_readsurf(surfType, subjCode, struPath)
 %
-% This function reads the FreeSurfer surface file (?h.white, ?h.pial,
-% ?h.inflated, ?h.sphere) with FreeSurfer Matlab functions (read_surf.m).
-% This function only further corrects the vertex numbers in the faces.
+% This function reads the FreeSurfer surface file with FreeSurfer Matlab 
+% functions (read_surf.m). This function further corrects the vertex 
+% numbers in the faces.
 %
 % Input:
-%    surfType       <string> the surface type (e.g., 'sphere') or the 
-%                    full path (or relative path) to the surface filename. 
-%    hemi           <string> 'lh' (default) or 'rh'.
-%    subjCode       <string> subject code in $SUBJECTS_DIR.
+%    surfFn         <string> the surface filename (e.g., 'lh.white') or the 
+%                    full path (or relative path) to the surface filename.
+%                    Default is lh.white.
+%    subjCode       <string> subject code in strucPath. Default is
+%                    fsaverage.
 %    struPath       <string> $SUBJECTS_DIR.
 %
 % Output:
@@ -19,37 +20,46 @@ function [coords, faces] = fs_readsurf(surfType, hemi, subjCode, struPath)
 %                    three columns are the indices of the three vertices
 %                    making that face.
 %
-% Dependency:
+% Surface files [faces for all these files should be the same]:
+%   ?h.white
+%   ?h.pial
+%   ?h.inflated
+%   ?h.sphere
+%
+% Dependency: 
 %    FreeSurfer Matlab functions.
+%
+% Example 1 (read a surf file in the current working directory):
+% fs_readsurf('./lh.white');
 %
 % Created by Haiyang Jin (30-March-2020)
 
-if nargin < 1 || isempty(surfType)
-    surfType = 'sphere';
+if ~exist('surfFn', 'var') || isempty(surfFn)
+    surfFn = 'lh.white';
+    warning('''%s'' is loaded by default.', surfFn);
 end
 
-surfPath = fileparts(surfType);
+% check if the path is available
+surfPath = fileparts(surfFn);
 
 if isempty(surfPath)
-    if nargin < 2 || isempty(hemi)
-        hemi = 'lh';
-    end
-    if nargin < 3 || isempty(subjCode)
+    if ~exist('subjCode', 'var') || isempty(subjCode)
         subjCode = 'fsaverage';
+        warning('''fsaverage'' is used as ''subjCode'' by default.');
     end
-    if nargin < 4 || isempty(struPath)
+    % use SUBJECTS_DIR as the default subject path
+    if ~exist('struPath', 'var') || isempty(struPath)
         struPath = getenv('SUBJECTS_DIR');
     end
     % the default path to surf/
-    surfPath = fullfile(struPath, subjCode, 'surf');
-    surfFilename = fullfile(surfPath, [hemi '.' surfType]);
+    surfFile = fullfile(struPath, subjCode, 'surf', surfFn);
 else
-    surfFilename = surfType;
+    surfFile = surfFn;
 end
 
 %% Read the surface file 
 % run read_surf.m from FreeSurfer Matlab functions
-[coords, faces] = read_surf(surfFilename);
+[coords, faces] = read_surf(surfFile);
 
 %% Correction the vertex indices
 % correct the vertex indices in faces (by plus 1)
