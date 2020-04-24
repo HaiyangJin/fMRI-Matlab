ff1cf40708952a325764d4a34e0e8cdb1c0da176
Haiyang <hjin317@aucklanduni.ac.nz>
2020-04-24 12:40:43 +0400

    show brain only (without data)

--- cvn/fs_cvn_lookup.m
+++ cvn/fs_cvn_lookup.m
@@ -13,12 +13,15 @@ function varargout= fs_cvn_lookup(trgSubj, view, valstruct, lookups, ...
 %    valstruct        <struct> struct('data',<L+R x 1>,'numlh',L,'numrh',R)
 %                      to create images for both hemispheres side by side.
 %                      In this case, hemi, view_az_el_tilt, and Lookup must
-%                      be cell arrays.
+%                      be cell arrays. [if valstruct is 'nodata', no data
+%                      will be displayed on the image (i.e., the brain
+%                      only); 'nodatalh' only displays left hemisphere; 
+%                      'nodatarh' only displays right hemisphere.]
 %    lookups          <struct> the Lookup to re-use.
 %    extraopts        <cell> a cell vector of extra options to
 %                      cvnlookupimages.m. Default: {}.
 %
-% Varargin:         
+% Varargin:
 %    'wantfig'        <logical/integer> whether to show a figure. 1: show
 %                      figure (default); 2: do not show figure, but output
 %                      himg.
@@ -29,13 +32,13 @@ function varargout= fs_cvn_lookup(trgSubj, view, valstruct, lookups, ...
 %                      e.g. 'fusiform' for 'aparc' (in 'annot').
 %    'thresh0'        <numeric> real number for numbers with sign and
 %                      non-real number is treated as abosolute threshold.
-%    'roimask'        <cell numeric> Vx1 binary mask (or cell array for 
+%    'roimask'        <cell numeric> Vx1 binary mask (or cell array for
 %                      multiple ROIs) for an ROI to draw on final RGB image.
-%                      For more plese check cvnlookupimages.m. IMPORTANT: 
+%                      For more plese check cvnlookupimages.m. IMPORTANT:
 %                      to display annotations, roi mask has to be set here.
 %    'roiwidth'       <numeric vector> width for the roi contour.
 %    'roicolor'       <numeric array> ColorSpec or RGB color for ROI outline(s).
-%    ...              For more, please check cvnlookupimages.m.   
+%    ...              For more, please check cvnlookupimages.m.
 %
 % Output (varargout):
 %    rawimg           <> We can directly call imagesc on rawimg. This might
@@ -56,12 +59,25 @@ function varargout= fs_cvn_lookup(trgSubj, view, valstruct, lookups, ...
 %                    second: frontal, occipital;
 %    3  vertical layout (one column): frontal, occipital;
 %
-% Example: show random activations on fsaverage with view 1
+%
+% % Example 1: show random activations on both hemispheres with view 1
 % fs_cvn_lookup('fsaverage', 1);
 %
+% % Example 2: show random activations on left hemisphere with view 1
+% valstruct.numlh = 163842;
+% valstruct.data = randn(valstruct.numlh,1);
+% fs_cvn_lookup('fsaverage', 1, valstruct);
+%
+% % Example 3: show brain only (no data)
+% fs_cvn_lookup('fsaverage', 1, 'nodata');
+%
+% % Example 4: show annotations without data
+% fs_cvn_lookup('fsaverage', 1, 'nodata', [], '', 'annot', 'aparc');
+%
 % Dependency:
-%    cvn codes: https://github.com/kendrickkay/knkutils.git
-%               https://github.com/kendrickkay/cvncode.git
+%    cvn codes from Kendrick Kay:
+%         https://github.com/kendrickkay/knkutils.git
+%         https://github.com/kendrickkay/cvncode.git
 %
 % Created by Haiyang Jin (13-Apr-2020)
 
@@ -73,11 +89,11 @@ defaultOpt=struct(...
     'annot', '',... % do not display annotation
     'annotwidth', 0.5,... % width of the annotation lines
     'annotname', '', ... % cell list of all annot areas to be displayed
-    'thresh0',0.0001i,...
+    'thresh0',[],...
     ...  % options in cvnlookupimages
     'roimask',[],...
     'roiwidth',{.5},...
-    'roicolor',{[]},...
+    'roicolor',{[1 1 1]},... % default white
     'surfsuffix','orig',...
     'rgbnan',0.5,... % the background color (gray)
     'hemiborder',2,...
@@ -88,17 +104,11 @@ defaultOpt=struct(...
 
 % parse options
 options=fs_mergestruct(defaultOpt, varargin{:});
-
 % Some other general setting
 thresh0 = options.thresh0;
-if isreal(thresh0)
-    threshopt = {'threshold',thresh0};
-else
-    threshopt = {'absthreshold',imag(thresh0)};
-end
-
-bgcolor = options.rgbnan;
 surfsuffix = options.surfsuffix;
+bgcolor = options.rgbnan;
+surftype = options.surftype;
 
 %%%%%%%%%%%%% View information %%%%%%%%%%%%%%%%
 viewStr = {'lmv', 'lmvfo', 'fo'};
@@ -126,10 +136,22 @@ end
 if ~exist('trgSubj', 'var') || isempty(trgSubj)
     trgSubj = 'fsaverage';
 end
+hemiInfo = {'numlh', 'numrh'};
 if ~exist('valstruct', 'var') || isempty(valstruct)
     % deal with valstruct data
     valstruct = valstruct_create(trgSubj,surfsuffix);
     valstruct.data = randn(size(valstruct.data));
+elseif ischar(valstruct) && startsWith(valstruct, 'nodata')
+    % do not show any data
+    isNoHemi = strcmp(['num' valstruct(7:end)], hemiInfo);
+    % create valstruct data with all zeros
+    valstruct = valstruct_create(trgSubj,surfsuffix);
+    %     valstruct = rmfield(valstruct, hemiInfo(isNoHemi));
+    if any(isNoHemi)
+        valstruct.(hemiInfo{isNoHemi}) = 0;
+        valstruct.data = valstruct.data(1: sum(valstruct.(hemiInfo{~isNoHemi})));
+    end
+    thresh0 = 1i;
 elseif ~isstruct(valstruct)
     error('Please make sure ''valstruct'' is struct.');
 end
@@ -138,9 +160,9 @@ if ~exist('extraopts','var') || isempty(extraopts)
 end
 
 % deal with viewhemis based on valstruct
-hemis = {'lh', 'rh'};
-isHemi = cellfun(@(x) valstruct.(x)~=0, {'numlh', 'numrh'});
-viewhemis = hemis(isHemi);
+isHemi = ismember(hemiInfo, fieldnames(valstruct));
+if all(isHemi); isHemi = cellfun(@(x) valstruct.(x)~=0, hemiInfo); end
+viewhemis = erase(hemiInfo(isHemi), 'num');
 
 % views for the hemi
 thisviewpt = cellfun(@(x) x(:, isHemi), viewpt{view, :}, 'uni', false);
@@ -156,6 +178,13 @@ if isnumeric(roimasks) && ~isempty(roimasks); roimasks = {roimasks}; end
 nRoi = numel(roimasks);
 roicolor = options.roicolor;
 roiwidth = options.roiwidth;
+% use roicolor and roiwidth for all rois if their numbers do not match
+if numel(roicolor) ~= nRoi
+    roicolor = repmat(roicolor, nRoi, 1);
+end
+if numel(roiwidth) ~= nRoi
+    roiwidth = repmat(roiwidth, nRoi, 1);
+end
 
 % load settings for adding annotations as extra masks
 annot = options.annot;
@@ -175,28 +204,36 @@ theAnnot = arrayfun(@(x) vertcat(annotCom{x, :}), 1:nAnnot, 'uni', false)';
 % annotation colors
 aUniColor = vertcat(aColor{:});
 aUniColor = aUniColor(1:nAnnot, :);
-
-%% generate image
-% process hemi related information
-if numel(viewhemis) == 1
-    surfdata = valstruct.data;
-    viewhemis = viewhemis{1};
-    thisviewpt = cellfun(@(x) x{1}, thisviewpt, 'uni', false);
-else
-    surfdata = valstruct;
+% annotwidth
+if numel(annotwidth) ~= nAnnot
+    annotwidth = repmat(annotwidth, nAnnot, 1);
 end
 
-% generate the image
-[~,lookup,rgbimgs] = cellfun(@(x, y) cvnlookupimages(trgSubj,...
-    surfdata, viewhemis, x, y,...
-    'surftype',options.surftype,...
-    'surfsuffix',surfsuffix,...
-    'rgbnan',bgcolor, ... %'text',upper(viewhemis),
-    'roimask',[roimasks;theAnnot], ...
-    'roicolor',[roicolor; aUniColor], ...
-    'roiwidth', [repmat(roiwidth, nRoi, 1); repmat(annotwidth, nAnnot, 1)], ...
-    'hemiborder',options.hemiborder,...
-    'hemibordercolor',options.hemibordercolor,... % set the border as gray
+%% Load the viewponits
+% hemi
+if numel(viewhemis) == 1
+    thisviewpt = cellfun(@(x) x{1}, thisviewpt, 'uni', false);
+end
+
+% [viewpt, viewhemis, viewhemi] = cvnlookupviewpoint(trgSubj,viewhemis,viewname,surftype);
+
+%% generate image
+if isreal(thresh0)
+    threshopt = {'threshold',thresh0};
+else
+    threshopt = {'absthreshold',imag(thresh0)};
+end
+
+[rawimg,lookup,rgbimgs] = cellfun(@(x, y) cvnlookupimages(trgSubj,...
+    valstruct, viewhemis, x, y,...
+    'surftype', surftype,...
+    'surfsuffix', surfsuffix,...
+    'rgbnan', bgcolor, ... %'text',upper(viewhemis),
+    'roimask', [roimasks;theAnnot], ...
+    'roicolor', [roicolor; aUniColor], ...
+    'roiwidth', [roiwidth; annotwidth], ...
+    'hemiborder', options.hemiborder,...
+    'hemibordercolor', options.hemibordercolor,... % set the border as gray
     'imageres', options.imageres, ...
     threshopt{:},extraopts{:}), ...
     thisviewpt, lookups, 'uni', false);
