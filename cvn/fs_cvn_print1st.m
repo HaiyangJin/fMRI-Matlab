de7871774892688d5fe2b8820b3e791439c3cba9
Haiyang <hjin317@aucklanduni.ac.nz>
2020-04-25 11:06:33 +0400

    [IMPORTANT UPDATE] use varargin and add more in-arguments

--- cvn/fs_cvn_print1st.m
+++ cvn/fs_cvn_print1st.m
@@ -1,4 +1,4 @@
-function fs_cvn_print1st(sessList, anaList, labelList, sigFn, thresh, outPath, funcPath)
+function fs_cvn_print1st(sessList, anaList, labelList, outPath, varargin)
 % fs_cvn_print1st(sessList, anaList, labelList, [sigFn='sig.nii.gz', thresh=1.301i...
 %    outPath=[pwd'/First_level_results'], funcPath])
 %
@@ -9,14 +9,26 @@ function fs_cvn_print1st(sessList, anaList, labelList, sigFn, thresh, outPath, f
 %    anaList         <cell string> list of analysis names.
 %    labelList       <cell string> list of label names or list of contrast
 %                     names.
-%    sigFn           <string> name of the to-be-printed file [Default is
-%                     sig.nii.gz].
-%    thresh0         <numeric> display threshold. overlayalpha =
-%                     ,val>threshold. Only show activation within the
-%                     threshold.
 %    outPath         <string> where to save the output images. [current
 %                     folder by default].
-%    funcPath        <string> the path to functional folder [Default is
+%
+% Optional inputs (varargin):
+%    'sigFn'         <string> name of the to-be-printed file [Default is
+%                     sig.nii.gz].
+%    'thresh'        <numeric> display threshold. Default is 1.3010 (abs).
+%    'clim'          <numeric array> limits for the color map. The Default
+%                     empty, which will display responses from %1 to 99%.
+%    'cmap'          <> use which color map, default is jet.
+%    'roicolors'     <numeric array> colors to be used for the label roi
+%                     masks.
+%    'markPeak'      <logical> mark the location of the peak response.
+%                     Default is 0.
+%    'showInfo'      <logical> show label information in the figure.
+%                     Default is 0, i.e., do not show the label information.
+%    'wantfig'       <logical/integer> Default is 2, i.e., do not show the
+%                     figure. More please check fs_cvn_lookup.
+%    'lookup'        <> setting used for cvnlookupimage.
+%    'funcPath'      <string> the path to functional folder [Default is
 %                     $FUNCTIONALS_DIR].
 %
 % Output:
@@ -28,6 +40,40 @@ function fs_cvn_print1st(sessList, anaList, labelList, sigFn, thresh, outPath, f
 waitHandle = waitbar(0, 'Preparing for printing first-level results...');
 
 %% Deal with intputs
+
+defaultOpts = struct(...
+    'sigFn', 'sig.nii.gz', ...
+    'thresh', 1.3010i, ...
+    'roicolors', {[1, 1, 1;  % white
+    1, 1, 0;  % yellow
+    1, 0, 1;  % Magenta / Fuchsia
+    0, 0, 1;  % blue
+    ]}, ...
+    'clim', [], ...
+    'cmap', jet(256), ...
+    'lookup', [], ...
+    'wantfig', 2, ...
+    'showInfo', 0, ...
+    'markPeak', 0, ... % mark the peak response in the label
+    'funcPath', getenv('FUNCTIONALS_DIR'), ...
+    'struPath', getenv('SUBJECTS_DIR'));
+
+options = fs_mergestruct(defaultOpts, varargin);
+
+% generate settings
+clim = options.clim;
+cmap = options.cmap;  % use jet(256) as the colormap
+lookup = options.lookup;
+wantfig = options.wantfig;  % do not show figure with fs_cvn_lookuplmv.m
+roicolors = options.roicolors;
+showInfo = options.showInfo;
+markPeak = options.markPeak;
+
+sigFn = options.sigFn;
+thresh = options.thresh;  % 0.05
+funcPath = options.funcPath;
+
+% some maybe nonsense default
 if ischar(sessList); sessList = {sessList}; end
 if ~exist('anaList', 'var') || isempty(anaList)
     anaList = {'loc_sm5_self.lh', 'loc_sm5_self.rh'};
@@ -40,34 +86,11 @@ elseif ischar(labelList)
     labelList = {labelList};
 end
 
-if ~exist('sigFn', 'var') || isempty(sigFn)
-    sigFn = 'sig.nii.gz';
-end
-if ~exist('thresh', 'var') || isempty(thresh)
-    thresh = 1.3010i;  % 0.05
-end
 % output path
 if ~exist('outPath', 'var') || isempty(outPath)
     outPath = fullfile(pwd, 'First_level_results');
 end
 % outPath = fullfile(outPath, sigFn);
-
-if ~exist('funcPath', 'var') || isempty(funcPath)
-    funcPath = getenv('FUNCTIONALS_DIR');
-end
-
-% generate settings
-clim0 = [];
-cmap0 = jet(256);  % use jet(256) as the colormap
-lookup = [];
-wantfig = 2;  % do not show figure with fs_cvn_lookuplmv.m
-% colors for roi contours
-roicolors = [
-    1, 1, 1;  % white
-    1, 1, 0;  % yellow
-    1, 0, 1;  % Magenta / Fuchsia
-    0, 0, 1;  % blue
-    ];
 
 nLabel = numel(labelList);
 nSess = numel(sessList);
@@ -133,46 +156,59 @@ for iLabel = 1:nLabel
             nVtx = numel(thisSurf.data);
             
             % set colormap limit based on the data if necessary
-            if isempty(clim0)
+            if isempty(clim)
                 thisclim0 = prctile(thisSurf.data(:),[1 99]);
             else
-                thisclim0 = clim0;
+                thisclim0 = clim;
             end
             
             % read the label and remove empty cells
-            thisMat = cellfun(@(x) fs_readlabel(x, subjCode), theseLabel, 'uni', false);
+            [thisMat, maxCell] = cellfun(@(x) fs_readlabel(x, subjCode), theseLabel, 'uni', false);
             isEmptyMat = cellfun(@isempty, thisMat);
             thisMat(isEmptyMat) = [];
+            maxCell(isEmptyMat) = [];
             
             % create roi mask for the label and roi name string
             maskStr = '';
             if isempty(thisMat)
-                thisRoi = {zeros(nVtx, 1)};
+                rois = []; 
+                roicolor = [];
+                nTheLabel = 1;
                 if endsWith(theLabelName, '.label')
                     maskStr = 'NoLabel || ';
                 end
             else
                 thisRoi = cellfun(@(x) makeroi(nVtx, x(:, 1)), thisMat, 'uni', false)';
+                nTheLabel = numel(thisRoi);
+                roicolor = roicolors(1:nTheLabel, :);
+                
+                % mark the peak in the label
+                if markPeak
+                    rois = [thisRoi; cellfun(@(x) makeroi(nVtx, x), maxCell, 'uni', false)'];
+                    roicolor = repmat(roicolor, 2, 1);
+                else
+                    rois = thisRoi;
+                end
             end
             
             % create parts of the output filename
-            nTheLabel = numel(thisRoi);
+            
             theLabelNames = theseLabel(~isEmptyMat);
             if all(isEmptyMat); theLabelNames = {theLabelName}; end
             labelNames = sprintf(['%s' repmat(' || %s', 1, nTheLabel-1)], theLabelNames{:});
             
-           
             % process the extra setting for printing
-            thisExtraopts = {'cmap',cmap0, 'clim', thisclim0};
+            thisExtraopts = {'cmap',cmap, 'clim', thisclim0};
             
+            %% Make the image
             %%%%%%% make image for this file %%%%%%%%
-            [~, lookup, rgbimg] = fs_cvn_lookup(trgSubj, -2, thisSurf, lookup, ... 
+            [~, lookup, rgbimg] = fs_cvn_lookup(trgSubj, -2, thisSurf, lookup, ...
                 'cvnopts', thisExtraopts, ...
                 'wantfig', wantfig, ...
-                'thresh0', thresh0, ...
-                'roimask', thisRoi, ...
-                'roicolor', roicolors(1:nTheLabel, :), ...
-                'roiwidth', ones(nTheLabel, 1), ...
+                'thresh', thresh0, ...
+                'roimask', rois, ...
+                'roicolor', roicolor, ...
+                'roiwidth', ones(numel(rois), 1), ...
                 'annot', 'aparc');
             
             % clear lookup if necessary
@@ -189,8 +225,30 @@ for iLabel = 1:nLabel
             imgName = sprintf('%s%s || %s', maskStr, labelNames, thisSess);
             set(fig, 'Name', imgName);
             
+            % Load and show the (first) label related information
+            if showInfo && ~all(isEmptyMat)
+                labelCell = cellfun(@(x) fs_labelinfo(x, subjCode), theLabelNames, 'uni', false);
+                labelTable = struct2table(vertcat(labelCell{:}));
+                labelTable.SubjCode = [];
+                
+                pos = get(fig, 'Position'); %// gives x left, y bottom, width, height
+                set(fig, 'Position', [pos(1:2) max(1050, pos(3)) pos(4)+400]);
+                % Get the table in string form.
+                TString = evalc('disp(labelTable)');
+                % Use TeX Markup for bold formatting and underscores.
+                TString = strrep(TString,'<strong>','\bf');
+                TString = strrep(TString,'</strong>','\rm');
+                TString = strrep(TString,'_','\_');
+                % Get a fixed-width font.
+                FixedWidth = get(0,'FixedWidthFontName');
+                % Output the table using the annotation command.
+                annotation(gcf,'Textbox','String',TString,'Interpreter','Tex',...
+                    'FontName',FixedWidth,'Units','Normalized',...
+                    'Position',[0 0 1 0.1],'FontSize',12,'LineStyle','none');
+            end
+            
             colorbar;
-            colormap(cmap0);
+            colormap(cmap);
             caxis(thisclim0);
             
             % print the figure
