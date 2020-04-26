a770c139a49f646700020e054eab0abc1b361a3d
Haiyang <hjin317@aucklanduni.ac.nz>
2020-04-26 20:31:45 +0400

    change the output of label information as a table and output the fscmd

--- freesurfer/fs_surfcluster.m
+++ freesurfer/fs_surfcluster.m
@@ -1,6 +1,6 @@
-function [roisize, talCoor, VtxMax, nVtx] = fs_surfcluster(sessCode, anaName,...
+function [labelTable, fscmd] = fs_surfcluster(sessCode, anaName,...
     labelFn, sigFn, thmin, outPath, funcPath, struPath)
-% [roisize, talCoor, VtxMax, nVtx] = fs_surfcluster(sessCode, anaName,...
+% [labelTable, fscmd] = fs_surfcluster(sessCode, anaName,...
 %     labelFn, [sigFn='sig.nii.gz', thmin=1.3, outPath=pwd, funcPath, struPath])
 %
 % This function obtains the size of the label (ROI) from FreeSurfer
@@ -17,15 +17,22 @@ function [roisize, talCoor, VtxMax, nVtx] = fs_surfcluster(sessCode, anaName,...
 %                      cluster. Default is sig.nii.gz.
 %    thmin            <string> the minimal threshold. Default is 1.3.
 %    outPath          <string> where the temporary output file is saved.
-%    funcPath         <string> the full path to the functional folder. 
+%    funcPath         <string> the full path to the functional folder.
 %    struPath         <string> $SUBJECTS_DIR.
 %
 % Outputs:
-%    roisize          <string> size in mm2.
-%    talCoor          <string> the talairach coordinates for the peak
-%                       (maybe).
-%    VtxMax           <string> the vertex number of the peak response.
-%    nVtx             <string> number of vertices in this label.
+%    labelTable       <table> includes information about the label file.
+%      .SubjCode       <cell> the input subjCode save as a cell.
+%      .AnalysisName   <cell> the analysis name save as a cell.
+%      .LabelName      <cell> the input labelFn (without path) but save as 
+%                       a cell.
+%      .ClusterNo      <integer> the number (index) of the cluster.
+%      .Max            <numeric> the peak response value.
+%      .VtxMax         <integer> vertex index of the peak response.
+%      .Size           <numeric> the size (area) of the label in mm^2.
+%      .MNI305         <1x3 numeric vector> coordinates (XYZ) of VtxMax in
+%                       MNI305 (fsaverage) space.
+%      .NVtxs          <integer> number of vertices in this label.
 %
 % Created by Haiyang Jin (18-Nov-2019)
 
@@ -60,11 +67,9 @@ if isLabel
     
     % make sure the label exists
     if isempty(fs_readlabel(labelFn, subjCode, struPath))
-        roisize = [];
-        talCoor = [];
-        VtxMax = [];
-        nVtx = [];
-        return; 
+        labelTable = [];
+        fscmd = '';
+        return;
     end
     fscmd_label = sprintf(' --clabel %s', fullfile(struPath, subjCode, ...
         'label', labelFn));
@@ -72,17 +77,17 @@ else
     conName = labelFn;
     fscmd_label = '';
 end
-    
+
 % sig file
 sigfile = fullfile(funcPath, sessCode, 'bold', anaName, conName, sigFn);
 
 % create the freesurfer command
-outFn = sprintf('cluster=%s=%s=%s.txt', anaName, conName, subjCode);
+outFn = sprintf('cluster=%s=%s=%s.txt', anaName, labelFn, subjCode);
 outFile = fullfile(outPath, outFn);
 
 % create and run FreeSurfer commands
 fscmd = sprintf(['mri_surfcluster --in %s --subject %s ' ...
-    '--surf white --thmin %d --hemi %s --sum %s%s'], ...
+    '--surf white --thmin %d --hemi %s --sum %s --nofixmni%s'], ...
     sigfile, subjCode, thmin, hemi, outFile, fscmd_label);
 isnotok = system(fscmd);
 assert(~isnotok, 'FreeSurfer commands (mri_surfcluster) failed.');
@@ -91,10 +96,21 @@ assert(~isnotok, 'FreeSurfer commands (mri_surfcluster) failed.');
 tempcell = importdata(outFile, ' ', 36);
 
 tempdata = tempcell.data;
+ClusterNo = tempdata(:, 1);
+Max = tempdata(:, 2);
+VtxMax = tempdata(:, 3);
+Size = tempdata(:, 4);
+MNI305 = tempdata(:, 5:7);
+NVtxs = tempdata(:, 8);
 
-VtxMax = tempdata(3);
-roisize = tempdata(4);
-talCoor = tempdata(5:7);
-nVtx = tempdata(8);
+% save inputs
+nCluster = numel(ClusterNo);
+SubjCode = repmat({subjCode}, nCluster, 1);
+AnalysisName = repmat({anaName}, nCluster, 1);
+[~, fn, ext] = fileparts(labelFn);
+LabelName = repmat({[fn ext]}, nCluster, 1);
+
+% save the out information as table
+labelTable = table(SubjCode, AnalysisName, LabelName, ClusterNo, Max, VtxMax, Size, MNI305, NVtxs);
 
 end
