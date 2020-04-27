165a660fb98f21fac37688cc339d823cb20cfc7c
Haiyang <hjin317@aucklanduni.ac.nz>
2020-04-27 20:23:41 +0400

    bug fixed

--- cvn/fs_cvn_print2nd.m
+++ cvn/fs_cvn_print2nd.m
@@ -113,7 +113,7 @@ for iSurf = 1: nSurf
     
     % generate figures for this pair
     [~, lookup, rgbimg] = fs_cvn_lookup('fsaverage', -2, valstruct, ...
-        lookup, 'cvnopts', extraopts, 'wantfig', wantfig, 'thresh0', thresh0);
+        lookup, 'cvnopts', extraopts, 'wantfig', wantfig, 'thresh', thresh0);
     
     % set the figure name and save it
     fig = figure('Visible','off');
