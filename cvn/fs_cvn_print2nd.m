5a2324c631016d9ebaf8f1482d46d18de2706271
Haiyang <hjin317@aucklanduni.ac.nz>
2020-04-28 09:40:54 +0400

    use varargin instead of extraopts

--- cvn/fs_cvn_print2nd.m
+++ cvn/fs_cvn_print2nd.m
@@ -1,4 +1,4 @@
-function fs_cvn_print2nd(sigPathInfo, sigFn, outPath, extraopts)
+function fs_cvn_print2nd(sigPathInfo, sigFn, outPath, varargin)
 % fs_cvn_print2nd(sigPath, [sigFn = 'perm.th30.abs.sig.cluster.nii.gz', ..
 %   outPath=pwd, extraopts={}])
 %
@@ -8,16 +8,31 @@ function fs_cvn_print2nd(sigPathInfo, sigFn, outPath, extraopts)
 % p-values before corrections.
 %
 % Inputs:
-%    sigPathInfo    <cell> a 1xQ or Px1 cell. All the path and filename
-%                    information to theto-be-printed files. Each row is
-%                    one layer (level) ofthe path and all the paths will
-%                    be combined in order(with all possible combinations).
-%                    [fileInfo will be dealt with fs_fullfile.m]
-%    sigFn          <string> the filename of the cluster p value file
-%                    (e.g., perm.th30.abs.sig.cluster.nii.gz by default).
-%    outPath        <string> where to save the output images. [current
-%                    folder by default].
-%    extraopts       extra options for cvnlookupimages.m
+%    sigPathInfo     <cell> a 1xQ or Px1 cell. All the path and filename
+%                     information to theto-be-printed files. Each row is
+%                     one layer (level) ofthe path and all the paths will
+%                     be combined in order(with all possible combinations).
+%                     [fileInfo will be dealt with fs_fullfile.m]
+%    sigFn           <string> the filename of the cluster p value file
+%                     (e.g., perm.th30.abs.sig.cluster.nii.gz by default).
+%    outPath         <string> where to save the output images. [current
+%                     folder by default].
+%
+% Optional inputs (varargin):
+%    'viewpt'        <integer> the viewpoitns to be used. More see
+%                     fs_cvn_lookup.m. Default is -2.
+%    'thresh'        <numeric> display threshold. Default is 1.3010 (abs).
+%    'clim'          <numeric array> limits for the color map. The Default
+%                     empty, which will display responses from %1 to 99%.
+%    'cmap'          <> use which color map, default is jet.
+%    'lookup'        <> setting used for cvnlookupimage.
+%    'annot'         <string> which annotation will be used. Default is
+%                     '', i.e., not display annotation file.
+%    'wantfig'       <logical/integer> Default is 2, i.e., do not show the
+%                     figure. More please check fs_cvn_lookup.
+%    'cvnopts'       <cell> extra options for cvnlookupimages.m.
+%    'funcPath'      <string> the path to functional folder [Default is
+%                     $FUNCTIONALS_DIR].
 %
 % Output:
 %    images of group-level results saved at outPath.
@@ -33,6 +48,30 @@ function fs_cvn_print2nd(sigPathInfo, sigFn, outPath, extraopts)
 % Created by Haiyang Jin (13-Apr-2020)
 
 %% Deal with input
+
+defaultOpts = struct(...
+    'viewpt', -2, ...
+    'thresh', 1.3010i, ...
+    'clim', [], ...
+    'cmap', jet(256), ...
+    'lookup', [], ...
+    'annot', '', ...
+    'wantfig', 2, ...
+    'cvnopts', {{}}, ...
+    'funcpath', getenv('FUNCTIONALS_DIR'), ... % not in use now
+    'strupath', getenv('SUBJECTS_DIR')); % not in use now
+
+options = fs_mergestruct(defaultOpts, varargin);
+
+
+viewpt = options.viewpt;
+thresh = options.thresh;  % absolute value of 1.3 (p = 0.05)
+clim = options.clim;
+cmap = options.cmap;  % use jet(256) as the colormap
+lookup = options.lookup;
+annot = options.annot;
+wantfig = options.wantfig;  % do not show figure with fs_cvn_lookuplmv.m
+cvnopts = options.cvnopts;
 
 % make the path
 sigPath = fs_fullfile(sigPathInfo{:});
@@ -65,10 +104,6 @@ end
 outPath = fullfile(outPath, sigFn);
 if ~exist(outPath, 'dir'); mkdir(outPath); end
 
-if ~exist('extraopts', 'var') || isempty(extraopts)
-    extraopts = {};
-end
-
 %% Read files
 % the full path to the sig files
 thefiles = fullfile(sigPath, sigFn);
@@ -86,20 +121,19 @@ surfs = [leftFiles, rightFiles];
 surfStruct = arrayfun(@(x) fs_cvn_valstruct(surfs(x, :)), ...
     1:size(surfs, 1), 'uni', false);
 
-% calculate the maximum of the absolute data values
-tempMax = max(cellfun(@(x) max(abs(x.data)), surfStruct));
-
-% use the minimum 5*Integer as the maximum limit
-climMax = ceil(tempMax/5)*5;
-
-% generate figures
-clim0 = [-climMax climMax];
-cmap0 = jet(256);  % use jet(256) as the colormap
-thresh0 = 1.3010i;  % absolute value of 1.3 (p = 0.05)
-lookup = [];
-wantfig = 2;  % do not show figure with fs_cvn_lookuplmv.m
-
-extraopts = [extraopts, {'cmap', cmap0, 'clim', clim0}];
+% Calculate the colorbar limit if it is empty
+if isempty(clim)
+    % calculate the maximum of the absolute data values
+    tempMax = max(cellfun(@(x) max(abs(x.data)), surfStruct));
+    
+    % use the minimum 5*Integer as the maximum limit
+    climMax = ceil(tempMax/5)*5;
+    
+    % generate figures
+    clim0 = [-climMax climMax];
+else
+    clim0 = clim;
+end
 
 %% Create figure for every surf
 nSurf = size(surfs, 1);
@@ -112,9 +146,12 @@ for iSurf = 1: nSurf
     valstruct = surfStruct{iSurf};
     
     % generate figures for this pair
-    [~, lookup, rgbimg] = fs_cvn_lookup('fsaverage', -2, valstruct, ...
-        lookup, 'cvnopts', extraopts, 'wantfig', wantfig, 'thresh', thresh0);
-    
+    [~, lookup, rgbimg] = fs_cvn_lookup('fsaverage', viewpt, valstruct, ...
+        lookup, 'cvnopts', [cvnopts, {'cmap', cmap, 'clim', clim0}], ...
+                'wantfig', wantfig, ...
+                'thresh', thresh, ...
+                'annot', annot); 
+            
     % set the figure name and save it
     fig = figure('Visible','off');
     imshow(rgbimg); % display lookup results (imagesc + colorbar)
@@ -124,7 +161,7 @@ for iSurf = 1: nSurf
     set(fig, 'Name', theConName{1});
     
     colorbar;
-    colormap(cmap0);
+    colormap(cmap);
     caxis(clim0);
     
     thisOut = fullfile(outPath, [theConName{1} '.png']);
