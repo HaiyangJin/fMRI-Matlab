b97c01eaa6acbf8346b59311b2a627b093422ee5
Haiyang <hjin317@aucklanduni.ac.nz>
2020-04-28 22:14:53 +0400

    new function to obtain the default colors

---
+++ utilities/fs_colors.m
@@ -0,0 +1,33 @@
+function outColor = fs_colors(nOutColor)
+% outColor = fs_colors(nColor)
+%
+% This function generates the default colors.
+% 
+% Input: 
+%    nColor       <integer> the number of colors want in the output.
+%
+% Output:
+%    outColor     <numeric array> nColor x 3. 
+%
+% Created by Haiyang Jin (28-Apr-2020)
+
+colors = [
+    1, 1, 1;  % white
+    1, 1, 0;  % yellow
+    1, 0, 1;  % Magenta / Fuchsia
+    0, 1, 0;  % green
+    .5, 0, 1;  % purple
+    0, 1, 1;  %
+    0, 0, 0;  % black
+    ];
+nColor = size(colors, 1);
+
+if ~exist('nOutColor', 'var') || isempty(nOutColor)
+    nOutColor = nColor;
+end
+
+colorIdx = mod((1:nOutColor)-1, nColor)+1;
+
+outColor = colors(colorIdx, :);
+
+end
