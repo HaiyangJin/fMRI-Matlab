c32215a4088b448bfc5e5131cde12988a89276f7
Haiyang <hjin317@aucklanduni.ac.nz>
2020-04-29 11:13:51 +0400

    now can run with multiple contrasts

--- freesurfer/fs_drawlabel.m
+++ freesurfer/fs_drawlabel.m
@@ -1,30 +1,34 @@
-function fs_drawlabel(sessList, analysisName, contrastName, fthresh, extraLabelInfo, funcPath)
-% fs_drawlabel(sessList, analysisName, contrastName, fthresh, extraLabelInfo, funcPath)
+function fscmd = fs_drawlabel(sessList, anaList, conList, fthresh, extraLabelInfo, funcPath)
+% fscmd = fs_drawlabel(sessList, anaList, conList, fthresh, extraLabelInfo, funcPath)
 %
 % This function use FreeSurfer ("tksurfer") to draw labels.
 %
-% Inputs: 
-%    sessList          <string> session code in $FUNCTIONALS_DIR;
-%                      <cell of string> cell of session codes in
-%                      %SUBJECTS_DIR.
-%    analysisName      <string> or <a cell of strings> the names of the
-%                      analysis (i.e., the names of the analysis folders).
-%    contrast_name     <string> contrast name used glm (i.e., the names of 
-%                      contrast folders).
-%    fthresh           <numeric> significance level (default is f13 (.05)).
-%    extraLabelInfo    <string> extra label information added to the end 
-%                      of the label name.
-%    funcPath          <string> the full path to the functional folder.
+% Inputs:
+%    sessList       <string> session code in $FUNCTIONALS_DIR;
+%                   <cell of string> cell of session codes in
+%                    %SUBJECTS_DIR.
+%    anaList        <string> or <a cell of strings> the names of the
+%                    analysis (i.e., the names of the analysis folders).
+%    conList        <string> contrast name used glm (i.e., the names of
+%                    contrast folders).
+%    fthresh        <numeric> significance level (default is f13 (.05)).
+%    extraLabelInfo  <string> extra label information added to the end
+%                     of the label name.
+%    funcPath       <string> the full path to the functional folder.
 %
 % Output:
+%    fscmd          <string> FreeSurfer commands used.
 %    a label saved in the label/ folder within $SUBJECTS_DIR
 %
 % Created by Haiyang Jin (10-Dec-2019)
 
-if ischar(sessList)
-    sessList = {sessList};
-end
+% convert to cell if it is char
+if ischar(sessList); sessList = {sessList}; end
 nSess = numel(sessList);
+if ischar(anaList); anaList = {anaList}; end
+nAna = numel(anaList);
+if ischar(conList); conList = {conList}; end
+nCon = numel(conList);
 
 if nargin < 4 || isempty(fthresh)
     fthresh = 2; % p < .01
@@ -39,31 +43,36 @@ if nargin < 6 || isempty(funcPath)
     funcPath = getenv('FUNCTIONALS_DIR');
 end
 
-% convert analysisName to cell if it is string
-if ischar(analysisName); analysisName = {analysisName}; end
-nAnalysis = numel(analysisName);
-
 %% Draw labels for all participants for both hemispheres
-
+fscmdCell = cell(nSess, nAna, nCon);
 for iSess = 1:nSess
     
     thisSess = sessList{iSess};
     subjCode = fs_subjcode(thisSess, funcPath);
     
-    for iAna = 1:nAnalysis
+    for iAna = 1:nAna
         
-        thisAna = analysisName{iAna};
+        thisAna = anaList{iAna};
         hemi = fs_2hemi(thisAna);
         
-        sigFile = fullfile(funcPath, thisSess, 'bold',...
-            thisAna, contrastName, 'sig.nii.gz');
-        
-        labelName = sprintf('roi.%s.f%d.%s.%slabel', ...
-            hemi, fthresh*10, contrastName, extraLabelInfo);
-        
-        % draw labels manually with FreeSurfer
+        for iCon = 1:nCon
+            
+            thisCon = conList{iCon};
+            
+            sigFile = fullfile(funcPath, thisSess, 'bold',...
+                thisAna, thisCon, 'sig.nii.gz');
+            
+            labelName = sprintf('roi.%s.f%d.%s.%slabel', ...
+                hemi, fthresh*10, thisCon, extraLabelInfo);
+            
+            % draw labels manually with FreeSurfer
             fscmdCell{iSess, iAna, iCon} = fv_drawlabel(subjCode, thisAna, sigFile, labelName, fthresh);
-        
-    end
-    
+            
+        end  % iCon
+    end  % iAna
+end  % iSess
+
+% make the FreeSurfer commands to one role
+fscmd = fscmdCell(:);
+
 end
