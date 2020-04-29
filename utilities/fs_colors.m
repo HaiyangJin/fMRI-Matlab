da09ba1697bcd45ca60553b7132dd63c68d08196
Haiyang <hjin317@aucklanduni.ac.nz>
2020-04-29 14:28:24 +0400

    comments updated

--- utilities/fs_colors.m
+++ utilities/fs_colors.m
@@ -9,6 +9,16 @@ function outColor = fs_colors(nOutColor)
 % Output:
 %    outColor     <numeric array> nColor x 3. 
 %
+% colors = [
+%     1, 1, 1;  % white
+%     1, 1, 0;  % yellow
+%     1, 0, 1;  % Magenta / Fuchsia
+%     0, 1, 0;  % green
+%     .5, 0, 1;  % purple
+%     0, 1, 1;  % blue
+%     0, 0, 0;  % black
+%     ];
+%
 % Created by Haiyang Jin (28-Apr-2020)
 
 colors = [
@@ -17,7 +27,7 @@ colors = [
     1, 0, 1;  % Magenta / Fuchsia
     0, 1, 0;  % green
     .5, 0, 1;  % purple
-    0, 1, 1;  %
+    0, 1, 1;  % blue
     0, 0, 0;  % black
     ];
 nColor = size(colors, 1);
