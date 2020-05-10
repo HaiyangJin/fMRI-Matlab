fcf8954d2ee3fe0cfed2d06045d1c528c0859480
Haiyang <hjin317@aucklanduni.ac.nz>
2020-05-10 15:05:33 +0400

    use 'hemi' when labelFn is not a string

--- utilities/fs_clusterlabel.m
+++ utilities/fs_clusterlabel.m
@@ -1,5 +1,5 @@
-function [clusterNo, nCluster, iterNo] = fs_clusterlabel(labelFn, subjCode, fmin)
-% [clusterNo, nCluster, iterNo] = fs_clusterlabel(labelFn, subjCode, fmin)
+function [clusterNo, nCluster, iterNo] = fs_clusterlabel(labelFn, subjCode, fmin, hemi)
+% [clusterNo, nCluster, iterNo] = fs_clusterlabel(labelFn, subjCode, fmin, hemi)
 %
 % This function assigns the vertices in one label into contiguous clusters.
 %
@@ -12,6 +12,8 @@ function [clusterNo, nCluster, iterNo] = fs_clusterlabel(labelFn, subjCode, fmin
 %    fmin          <numeric> The (absolute) minimum value for vertices to
 %                   be used for assigning a cluster index. Default is 0,
 %                   i.e., all vertices will be used.
+%    hemi          <string> 'lh' or 'rh'. hemi will be used when labelFn is
+%                   a numeric array. Which hemisphere is labelFn from.
 %
 % Output:
 %    clusterNo     <integer vector> Px1 intger. Cluster index for each
@@ -33,15 +35,20 @@ end
 if ischar(labelFn)
     % read the label
     labelMat = fs_readlabel(labelFn, subjCode);
+    hemi = fs_2hemi(labelFn);
 else
     labelMat = labelFn;
+    assert(logical(exist('hemi', 'var')), ['Please define ''hemi'' when '''...
+        'labelFn'' is a numeric matrix.']);
 end
 
 if size(labelMat, 2) >= 5
     % find vertices larger than fmin
     isCluster = abs(labelMat(:, 5)) >= fmin;
+    vtxValue = labelMat(isCluster, 5);
 else
     isCluster = true(size(labelMat, 1), 1);
+    vtxValue = [];
 end
 
 % return if no vertices are larger than fmin
@@ -55,14 +62,13 @@ end
 
 %% Assign the cluster indices
 % keep the labelMat for the cluster vertices
-clusterMat = labelMat(isCluster, :);
-clusterVtx = clusterMat(:, 1);
+clusterVtx = labelMat(isCluster, 1);
 
 % obtain the neighborhood vertices
-nbrVtx = fs_neighborvtx(clusterVtx, subjCode);
+nbrVtx = fs_neighborvtx(clusterVtx, hemi, subjCode);
 
 % assign the vertices into clusters
-[theClusterNo, nCluster, theIterNo] = fs_clustervtx(clusterVtx, nbrVtx);
+[theClusterNo, nCluster, theIterNo] = fs_clustervtx(clusterVtx, nbrVtx, vtxValue);
 
 %% Save the cluster index information
 % create vector for all vertices (with -1)
