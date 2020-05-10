fdcaa66542d1247a4c0f96b26aebace0c22a6187
Haiyang <hjin317@aucklanduni.ac.nz>
2020-05-10 13:38:46 +0400

    add in-argument 'vtxIdx' for calculate the area for only these vertices

--- utilities/fs_labelarea.m
+++ utilities/fs_labelarea.m
@@ -1,5 +1,5 @@
-function labelarea = fs_labelarea(labelFn, subjCode, struPath)
-% labelarea = fs_labelarea(labelFn, subjCode, struPath)
+function labelarea = fs_labelarea(labelFn, subjCode, vtxIdx, struPath)
+% labelarea = fs_labelarea(labelFn, subjCode, vtxIdx, struPath)
 %
 % This function calculates the area (mm^2) for the label file. [It should
 % be the area on the ?h.white surface (I guess)].
@@ -9,6 +9,8 @@ function labelarea = fs_labelarea(labelFn, subjCode, struPath)
 %                     path). If path is included in labelFn, 'subjCode'
 %                     and struPath will be ignored. 
 %    subjCode        <string> subject code in struPath. Default is empty.
+%    vtxIdx          <integer array> indices of vertices whose areas will
+%                     be calculated. Default is all vertices in the label.
 %    struPath        <string> $SUBJECTS_DIR.
 %
 % Output:
@@ -43,7 +45,12 @@ trgSubj = fs_trgsubj(subjCode, template);
 % areas for all vertices
 area = fs_readcurv(curvFile, trgSubj, struPath);
 
+% calculate the all vertices by default
+if ~exist('vertices', 'var') || isempty(vtxIdx)
+    vtxIdx = labelMat(:, 1);
+end
+
 % sum the areas of vertices in the label
-labelarea = sum(area(labelMat(:, 1)));
+labelarea = sum(area(vtxIdx));
 
 end
