b6c2c33b1f1d679867a6530223b6bdc7f2157098
Haiyang <hjin317@aucklanduni.ac.nz>
2020-05-23 20:09:43 +0400

    use cellfun instead of for loops

--- freesurfer/fs_drawlabel.m
+++ freesurfer/fs_drawlabel.m
@@ -1,4 +1,4 @@
-function fscmd = fs_drawlabel(sessList, anaList, conList, fthresh, extraLabelInfo, runcmd, funcPath)
+function fscmd = fs_drawlabel(sessList, anaList, conList, fthresh, extraLabelStr, runcmd, funcPath)
 % fscmd = fs_drawlabel(sessList, anaList, conList, fthresh, extraLabelInfo, runcmd, funcPath)
 %
 % This function use FreeSurfer ("tksurfer") to draw labels.
@@ -12,7 +12,7 @@ function fscmd = fs_drawlabel(sessList, anaList, conList, fthresh, extraLabelInf
 %    conList        <string> contrast name used glm (i.e., the names of
 %                    contrast folders).
 %    fthresh        <numeric> significance level (default is 2 (.01)).
-%    extraLabelInfo  <string> extra label information added to the end
+%    extraLabelStr  <string> extra label information added to the end
 %                     of the label name.
 %    funcPath       <string> the full path to the functional folder.
 %    runcmd         <logical> 1: run FreeSurfer commands; 0: do not run
@@ -26,60 +26,53 @@ function fscmd = fs_drawlabel(sessList, anaList, conList, fthresh, extraLabelInf
 
 % convert to cell if it is char
 if ischar(sessList); sessList = {sessList}; end
-nSess = numel(sessList);
 if ischar(anaList); anaList = {anaList}; end
-nAna = numel(anaList);
 if ischar(conList); conList = {conList}; end
-nCon = numel(conList);
 
 if ~exist('fthresh', 'var') || isempty(fthresh)
-    fthresh = 2; % p < .01
+    fthresh = {2}; % p < .01
+elseif ~iscell(fthresh)
+    fthresh = num2cell(fthresh);
 end
-if ~exist('extraLabelInfo', 'var') || isempty(extraLabelInfo)
-    extraLabelInfo = '';
-elseif ~strcmp(extraLabelInfo(end), '.')
-    extraLabelInfo = [extraLabelInfo, '.'];
+
+if ~exist('extraLabelStr', 'var') || isempty(extraLabelStr)
+    extraLabelStr = {''};
+elseif ischar(extraLabelStr)
+    extraLabelStr = {extraLabelStr};
 end
 
 if ~exist('runcmd', 'var') || isempty(runcmd)
     runcmd = 1;
 end
-
 if ~exist('funcPath', 'var') || isempty(funcPath)
     funcPath = getenv('FUNCTIONALS_DIR');
 end
 
-
 %% Draw labels for all participants for both hemispheres
-fscmdCell = cell(nSess, nAna, nCon);
-for iAna = 1:nAna
-    
-    thisAna = anaList{iAna};
-    hemi = fs_2hemi(thisAna);
-    
-    for iSess = 1:nSess
-        
-        thisSess = sessList{iSess};
-        subjCode = fs_subjcode(thisSess, funcPath);
-        
-        
-        for iCon = 1:nCon
-            
-            thisCon = conList{iCon};
-            
-            sigFile = fullfile(funcPath, thisSess, 'bold',...
-                thisAna, thisCon, 'sig.nii.gz');
-            
-            labelName = sprintf('roi.%s.f%d.%s.%slabel', ...
-                hemi, fthresh*10, thisCon, extraLabelInfo);
-            
-            % draw labels manually with FreeSurfer
-            fscmdCell{iSess, iAna, iCon} = fv_drawlabel(subjCode, thisAna, sigFile, labelName, fthresh, runcmd);
-            
-        end  % iCon
-    end  % iSess
-end  % iAna
+% create all the combinations
+[theExtra, theSess, theAna, theThesh, theCon] = ...
+    ndgrid(extraLabelStr, sessList, anaList, fthresh, conList);
 
+ana = theAna(:);
+sess = theSess(:);
+con = theCon(:);
+thresh = theThesh(:);
+extraStr = theExtra(:);
+
+% add '.' at the end if necessary
+notdot = cellfun(@(x) ~endsWith(x, '.') & ~isempty(x), extraStr);
+extraStr(notdot) = cellfun(@(x) [x '.'], extraStr(notdot), 'uni', false);
+
+% obtain necessary information
+hemi = cellfun(@fs_2hemi, ana, 'uni', false);
+subjCode = cellfun(@(x) fs_subjcode(x, funcPath), sess, 'uni', false);
+sigFile = fullfile(funcPath, sess, 'bold', ana, con, 'sig.nii.gz');
+labelName = cellfun(@(x1, x2, x3, x4) sprintf('roi.%s.f%d.%s.%slabel', ...
+    x1, x2*10, x3, x4), hemi, thresh, con, extraStr, 'uni', false);
+
+% create labels
+fscmdCell = cellfun(@(x1, x2, x3, x4, x5) fv_drawlabel(x1, x2, x3, x4, x5, runcmd), ...
+    subjCode, ana, sigFile, labelName, thresh, 'uni', false);
 
 % make the FreeSurfer commands to one role
 fscmd = fscmdCell(:);
