d7b1898042f66645d30cc2dde26710b7ee9f1f7c
Haiyang <hjin317@aucklanduni.ac.nz>
2020-10-13 09:36:34 +0400

    update function to use fs_vtxarea.m

--- utilities/fs_labelarea.m
+++ utilities/fs_labelarea.m
@@ -1,5 +1,5 @@
-function [labelarea, vtxarea] = fs_labelarea(labelFn, subjCode, vtxIdx, struPath)
-% labelarea = fs_labelarea(labelFn, subjCode, vtxIdx, struPath)
+function labelarea = fs_labelarea(labelFn, subjCode, struPath)
+% labelarea = fs_labelarea(labelFn, subjCode, struPath)
 %
 % This function calculates the area (mm^2) for the label file. [It should
 % be the area on the ?h.white surface (I guess)].
@@ -9,17 +9,19 @@ function [labelarea, vtxarea] = fs_labelarea(labelFn, subjCode, vtxIdx, struPath
 %                     path). If path is included in labelFn, 'subjCode'
 %                     and struPath will be ignored. 
 %    subjCode        <string> subject code in struPath. Default is empty.
-%    vtxIdx          <integer array> indices of vertices whose areas will
-%                     be calculated. Default is all vertices in the label.
 %    struPath        <string> $SUBJECTS_DIR.
 %
 % Output:
 %    labelarea       <numeric> the label area in mm^2.
 %
 % Created by Haiyang Jin (22-Apr-2020)
+%
+% See also:
+% fs_vtxarea
 
 if ~exist('subjCode', 'var') || isempty(subjCode)
-    subjCode = '';
+    subjCode = 'fsaverage';
+    warning('''fsaverage'' is used as ''subjCode'' by default.');
 end
 if ~exist('struPath', 'var') || isempty(struPath)
     struPath = '';
@@ -30,33 +32,14 @@ labelMat = fs_readlabel(labelFn, subjCode, struPath);
 
 if isempty(labelMat)
     labelarea = 0;
-    vtxarea = [];
     return;
 end
 
-% decide the hemisphere for the label file
-hemi = fs_2hemi(labelFn);
-curvFile = [hemi '.area'];
-
-% add path to curvFile if path is included in labelFn
-labelPath = fileparts(labelFn);
-if ~isempty(labelPath)
-    curvFile = fullfile(labelPath, '..', 'surf', curvFile);
-end
-
-% read the area for fsaverage if the label is on fsaverage surface
-template = fs_2template(labelFn, '', 'self');
-trgSubj = fs_trgsubj(subjCode, template);
-
-% areas for all vertices
-vtxarea = fs_readcurv(curvFile, trgSubj, struPath);
-
 % calculate the all vertices by default
-if ~exist('vtxIdx', 'var') || isempty(vtxIdx)
-    vtxIdx = labelMat(:, 1);
-end
+vtxIdx = labelMat(:, 1);
+hemi = fs_2hemi(labelFn);
 
-% sum the areas of vertices in the label
-labelarea = sum(vtxarea(vtxIdx));
+% calculate the area
+labelarea = fs_vtxarea(vtxIdx, subjCode, hemi, struPath);
 
 end
