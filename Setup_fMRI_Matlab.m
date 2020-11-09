20b9524dab0feeef9a2b11d2e51790faccfb61d2
Haiyang <hjin317@aucklanduni.ac.nz>
2020-11-09 17:16:17 +0400

    add function to add path

---
+++ Setup_fMRI_Matlab.m
@@ -0,0 +1,11 @@
+function Setup_fMRI_Matlab
+% This function adds the necessary folders to Matlab path.
+
+dirList = dir;
+dirList([dirList.isdir]==0) = []; 
+dirList(cellfun(@(x) startsWith(x, '.'), {dirList.name})) = []; 
+dirList(ismember({dirList.name}, {'documents', 'reflabels'})) = [];
+
+addpath(genpath(dirList));
+
+end
