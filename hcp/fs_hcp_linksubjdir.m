56375eb53cc82c59393efb2950f87198da41cd04
Haiyang <hjin317@aucklanduni.ac.nz>
2021-10-18 09:22:28 +0400

    fix bug

--- hcp/fs_hcp_linksubjdir.m
+++ hcp/fs_hcp_linksubjdir.m
@@ -1,11 +1,11 @@
-function fs_hcp_linksubjdir(structPath, hcpDir, linkT1)
+function fs_hcp_linksubjdir(struPath, hcpDir, linkT1)
 % fs_hcp_linksubjdir(structPath, hcpDir, linkT1)
 %
 % This function re-link the SUBJECTS_DIR after the output from HCP is
 % downloaded to local storage.
 %
 % Input:
-%     structPath      <string> '$SUBJECTS_DIR'
+%     struPath        <string> '$SUBJECTS_DIR'
 %     hcpDir          <string> the full path to HCP/ folder, which stores
 %                       subject folders.
 %     linkT1          <logical> 1 (default): link files. 0: copy files.
@@ -16,10 +16,13 @@ function fs_hcp_linksubjdir(structPath, hcpDir, linkT1)
 % Created by Haiyang Jin (20-Jan-2020)
 
 % gather information about 'SUBJECTS_DIR'
-if ~exist('structPath', 'var')  || isempty(structPath)
-    structPath = fs_subjdir;
+if ~exist('struPath', 'var')  || isempty(struPath)
+    struPath = fs_subjdir;
 end
 % path to SUBJECTS_DIR
+if ~endsWith(struPath, filesep)
+    struPath = [struPath filesep];
+end
 
 % obtain the  the hcpDir (if empty) based on structPath
 if ~exist('hcpDir', 'var')  || isempty(hcpDir)
@@ -37,12 +40,13 @@ for iSubj = 1:length(subjList)
     thisSubj = subjList{iSubj};
     
     sourceSubjCode = fullfile(hcpDir, thisSubj, 'T1w', thisSubj);
-    targetSubjCode = fullfile(structPath, thisSubj);
+    targetSubjCode = fullfile(struPath, thisSubj);
     
     % link (or copy) recon-all data
     if linkT1 % link files
-        delete(targetSubjCode);  % delete the old folder
-        cmd_link = sprintf('ln -s %s %s', sourceSubjCode, structPath);
+        % delete the old folder
+        if logical(exist(targetSubjCode, 'dir')); delete(targetSubjCode); end
+        cmd_link = sprintf('ln -s %s %s', sourceSubjCode, struPath);
         system(cmd_link);
     elseif ~linkT1
         copyfile(sourceSubjCode, targetSubjCode);
