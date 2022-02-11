59e0ee05135930dc391e738120c94b0ea5681272
Haiyang <hjin317@aucklanduni.ac.nz>
2022-02-11 10:39:54 +0400

    add hires option

--- freesurfer/fs_recon.m
+++ freesurfer/fs_recon.m
@@ -1,5 +1,5 @@
-function [fscmd, isok] = fs_recon(t1list, subjCode, t2list, runcmd)
-% [fscmd, isok] = fs_recon(t1File, subjCode, t2File, runcmd)
+function [fscmd, isok] = fs_recon(t1list, subjCode, t2list, hires, runcmd)
+% [fscmd, isok] = fs_recon(t1File, subjCode, t2File, hires, runcmd)
 %
 % This function run 'recon-all' in FreeSurfer.
 %
@@ -9,6 +9,8 @@ function [fscmd, isok] = fs_recon(t1list, subjCode, t2list, runcmd)
 %    subjCode     <str> subject code.
 %    t2list       <str> (optional) the path to the T2 file.
 %              OR <cell str> a list of T2 files.
+%    hires        <boo> whether run recon-all with native high resolution.
+%                   default is 0.
 %    runcmd       <boo> whether to run the recon-all commands [default: 1].
 %
 % Output:
@@ -34,13 +36,32 @@ elseif ischar(t2list)
 elseif iscell(t2list)
     t2cmd = sprintf('-T2 %s ', t2list{:});
 end
+if ~isempty(t2cmd)
+    t2cmd = [t2cmd '-T2pial '];
+end
+
+if ~exist('hires', 'var') || isempty(hires)
+    hires = 0;
+end
+
+% cmd for run with native high resolution
+if hires
+    hicmd = '-hires';
+%     hicmd = sprintf('-hires -expert %s ', fullfile( ...
+%         fm_2cmdpath(fileparts(mfilename('fullpath'))), 'expert.opts'));
+else
+    hicmd = '';
+end
 
 if ischar(t1list)
     t1list = {t1list};
 end
 
-fscmd = sprintf('recon-all%s -s %s %s-all', ...
-    sprintf(' -i %s', t1list{:}), subjCode, t2cmd);
+fscmd = sprintf('recon-all -s %s %s %s%s  -all', ...
+    subjCode, sprintf(' -i %s', t1list{:}), t2cmd, hicmd);
+
+% display the recon-all command
+fprintf('\n%s \n', fscmd);
 
 if runcmd
     isnotok = system(fscmd);
