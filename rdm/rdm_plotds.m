938c54bb6eedfdbcb35c4f027cdc13e5b42a2849
Haiyang <hjin317@aucklanduni.ac.nz>
2022-08-22 11:55:51 +0400

    new func to plot RDMs in ds

---
+++ rdm/rdm_plotds.m
@@ -0,0 +1,26 @@
+function rdm_plotds(ds, varargin)
+% rdm_plotds(ds, varargin)
+%
+% Use rdm_plotrdm() to plot RDM vectors in ds. 
+%
+% Inputs:
+%     ds       <struct> RDM dataset.
+%     
+% Varargin:
+%     Please see Varargin in rdm_plotrdm().
+%
+% Output
+%
+% Created by Haiyang Jin (2022-Aug-22)
+
+if nargin < 1
+    fprintf('rdm_plotds(ds, varargin);\n');
+end
+
+% convert ds.samples to RDM (matrix from vector)
+rdms = rdm_vec2rdm(ds.samples, 'lower', 0.5);
+
+% plot
+rdm_plotrdm(rdms, 'condnames', ds.a.conditions, 'titles', ds.sa.labels, varargin{:});
+
+end
