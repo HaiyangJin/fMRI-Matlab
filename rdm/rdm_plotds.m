754efc783940ccdb182c890b9f0bf660e448bfe0
Haiyang <hjin317@aucklanduni.ac.nz>
2022-08-24 10:51:56 +0400

    add diagvalue to varargin

--- rdm/rdm_plotds.m
+++ rdm/rdm_plotds.m
@@ -1,14 +1,14 @@
-function rdm_plotds(ds, diagValue, varargin)
+function rdm_plotds(ds, varargin)
 % rdm_plotds(ds, varargin)
 %
 % Use rdm_plotrdm() to plot RDM vectors in ds. 
 %
 % Inputs:
 %     ds            <struct> RDM dataset.
-%     diagValue     <num> value to be displayed on the diagnoal. Default to
-%                    0. 
 %     
 % Varargin:
+%     .diagvalue    <num> value to be displayed on the diagnoal. Default 
+%                    to 0. 
 %     Please see Varargin in rdm_plotrdm().
 %
 % Created by Haiyang Jin (2022-Aug-22)
@@ -17,12 +17,12 @@ if nargin < 1
     fprintf('Usage: rdm_plotds(ds, varargin);\n');
 end
 
-if ~exist('diagValue', 'var') || isempty(diagValue)
-    diagValue = 0;
-end
+defaultOptions = struct( ...
+    'diagvalue', 0);
+opts = fm_mergestruct(defaultOptions, varargin);
 
 % convert ds.samples to RDM (matrix from vector)
-rdms = rdm_vec2rdm(ds.samples, diagValue);
+rdms = rdm_vec2rdm(ds.samples, opts.diagvalue);
 
 % plot
 rdm_plotrdm(rdms, 'condnames', ds.a.conditions, 'titles', ds.fa.labels, varargin{:});
