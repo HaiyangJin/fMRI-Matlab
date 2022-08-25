64bf14c6a23fcfaa775e61203b4024848a7e4e0c
Haiyang <hjin317@aucklanduni.ac.nz>
2022-08-25 11:01:01 +0400

    make either row or column vectors

--- rdm/rdm_ds.m
+++ rdm/rdm_ds.m
@@ -38,21 +38,30 @@ ds_rdm.samples = samples;
 if ~isempty(opts.condlist)
     assert(nchoosek(length(opts.condlist),2)==size(samples,1), ['The length' ...
         ' of {.condlist} does not seem to match what it should be for {size(samples,1)}.'])
-
+    % make it to (column) vector
+    if size(opts.condlist, 1) == 1
+        opts.condlist = opts.condlist';
+    end
     ds_rdm.a.conditions = opts.condlist;
 end
 
 if ~isempty(opts.labellist)
     assert(length(opts.labellist)==size(samples, 2), ['The length of ' ...
         '{.labellist} does not seem to match {size(samples,2)}']);
-
+    % make it to row vector
+    if size(opts.labellist, 2) == 1
+        opts.labellist = opts.labellist';
+    end
     ds_rdm.fa.labels = opts.labellist;
 end
 
 if ~isempty(opts.subjlist)
     assert(length(opts.subjlist)==size(samples, 3), ['The length of ' ...
         '{.subjlist} does not seem to match {size(samples,3)}.']);
-
+    % make it to (column) vector
+    if size(opts.subjlist, 1) == 1
+        opts.subjlist = opts.subjlist';
+    end
     ds_rdm.pa.labels = opts.subjlist;
 end
 
