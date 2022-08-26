0fef29ac056a7dd4baaf2eb3e575363f82f863fc
Haiyang <hjin317@aucklanduni.ac.nz>
2022-08-26 12:47:00 +0400

    upgrade methods

--- rsa/rsa_noiseceiling.m
+++ rsa/rsa_noiseceiling.m
@@ -1,70 +1,125 @@
-function noise = rsa_noiseceiling(rdms, meanout)
-% noise = rsa_noiseceiling(rdms, meanout)
+function [ub, lb] = rsa_noiseceiling(ds_subj, type, method)
+% [ub, lb] = rsa_noiseceiling(ds_subj, type, method)
 %
 % Calculate the noise ceiling for RDMs. The lower bound of the noise 
-% ceiling was estimated by calculating the Pearson correlation of the brain
+% ceiling was estimated by calculating the correlation of the brain
 % RDM for each participant with the average brain RDM across all other 
-% participants (after z scoring the brain RDM for each participant). 
+% participants (after the appropriate transformation). 
 % The upper bound of the noise ceiling was estimated by computing the 
-% Pearson correlation of the brain RDM for each participant with the 
-% average brain RDM across all participants (after z scoring the brain RDM 
-% for each participant). More see Reference.
+% correlation of the brain RDM for each participant with the 
+% average brain RDM across all participants (after the appropriate 
+% transformation). More see Reference.
 %
 % Inputs:
-%    rdms        <num array> P x P x N array. P x P is a RDM for one
-%                 participant and there are N participants in total.
-%             OR <num array> Q x N array. Q is the number of pairs in RDM and
-%                 N is the number of participants.
-%    meanout     <str> whether additional steps are applied to the output
-%                 cor. 'mean': directly get the mean of all values in cor;
+%    ds_subj     <struct> subject RDMs. Each column in .samples is one brain
+%                 RDM (vector). The third dimension is the participant.
+%    type        <str> correlation to be used to compare RDMs. Default to
+%                 'kendall_taua' (from rsatoolbox). Other options are
+%                 methods avaiable in corr in matlab (statistics toolbox).
+%                 more see rsa_corr().
+%    method     <str> whether additional steps are applied to the output
+%                 cor. 'mean': directly get the mean of upper and lower boundaries;
 %                 'tanh': apply atanh, calculate the mean, and apply tanh;
-%                 'raw': do nothing.
+%                 'raw': do nothing (just output the matrix).
 %
 % Output:
-%    noise       <num> 1 x 2 vector; the lower and upper boundary of the 
-%                 noise ceiling.
+%    ub           <num> The upper boundary of the noise ceiling.
+%    lb           <num> The lower boundary of the noise ceiling.
 %
 % Reference:
 % Nili, H., Wingfield, C., Walther, A., Su, L., Marslen-Wilson, W., &
 %   Kriegeskorte, N. (2014). A Toolbox for Representational Similarity
 %   Analysis. PLoS Computational Biology, 10(4), e1003553.
 %   https://doi.org/10.1371/journal.pcbi.1003553
-% Tsantani, M., Kriegeskorte, N., Storrs, K., Williams, A. L., McGettigan, 
-%   C., & Garrido, L. (2021). FFA and OFA Encode Distinct Types of Face 
-%   Identity Information. The Journal of Neuroscience, 41(9), 1952–1969. 
-%   https://doi.org/10.1523/JNEUROSCI.1449-20.2020
 %
 % Created by Haiyang Jin (2021-11-15)
 
 if nargin < 1
-    fprintf('Usage: noise = rsa_noiseceiling(rdms, meanout);\n');
+    fprintf('Usage: [ub, lb] = rsa_noiseceiling(ds_subj, type, method);\n');
     return
 end
 
-% output
-noise = NaN(1, 2);
-
-if ndims(rdms) == 3
-    [P1, P2, N] = size(rdms);
-    assert(P1==P2, ['The first two dimensions of <rdms> have to be the same' ...
-        ' when there are 3 dimensions in <rdms>.'])
-
-    % from 3d to 2d: save the upper right corner of rdms as one row vector
-    boo_triu = logical(triu(ones(P1),1));
-    rdms = reshape(rdms(repmat(boo_triu, 1, 1, N)), [], N)';
-elseif ndims(rdms) > 3
-    error('The dimension of <rdms> should be 2 or 3.')
+if ~exist('type', 'var') || isempty(type)
+    type = 'kendall_taua';
 end
 
-if ~exist('meanout', 'var') || isempty(meanout)
-    meanout = 'mean';
+if ~exist('method', 'var') || isempty(method)
+    method = 'mean';
 end
 
-% Standardized rdms for each participant separately (column)
-z_rdms = zscore(rdms, [], 1);
+% convert samples to cell
+sample_cell = num2cell(ds_subj.samples, 1);
 
-% noise ceiling
-noise(1) = rsa_reliability(z_rdms, 0, meanout);
-noise(2) = rsa_reliability(z_rdms, 1, meanout);
+%% Apply transformations
+switch type
+    case {'kendall_taua', 'Spearman'}
+        % rank-transform
+        sample_cell = cellfun(@tiedrank, sample_cell, 'uni', false);
 
-end
+    case 'Pearson'
+        % z-transformed
+        sample_cell = cellfun(@zscore, sample_cell, 'uni', false);
+end %switch type
+
+samples = cell2mat(sample_cell);
+
+%% Noise ceiling - upper boundaries
+sample_avg = mean(samples, 3, 'omitnan');
+
+% correlation between the average RDM and subject RDM
+corr_cell = arrayfun(@(x) ...
+    rsa_corr(squeeze(samples(:,x,:)), sample_avg(:,x), type), ...
+    1:size(sample_avg, 2), 'uni', false);
+
+corr_ub = vertcat(corr_cell{:})';
+
+% apply transform if needed
+ub = transformmean(corr_ub, method);
+
+%% Noise ceiling - lower boundaries
+corr_lb = nan(size(samples, 3), size(samples, 2));
+
+for iSubj = 1:size(samples, 3)
+    
+    % leave-one-out mean
+    tmpsample = samples;
+    tmpsample(:,:,iSubj) = [];
+    tmpavg = mean(tmpsample, 3, 'omitnan'); 
+
+    % leave-out-out correlation
+    corr_lb(iSubj,:) = arrayfun(@(x) ...
+        rsa_corr(squeeze(samples(:,x,iSubj)), tmpavg(:,x), type), ...
+        1:size(tmpavg, 2));
+    
+end %iSubj
+
+% apply transform if needed
+lb = transformmean(corr_lb, method);
+
+
+end %function
+
+function cor = transformmean(corr_mat, method)
+% corr_mat: each column is one reference model and each row is one
+% participant.
+
+% apply tansformation to the output
+switch method
+    case 'mean'
+        % do not apply any transformation
+        cor = mean(corr_mat, 1, 'omitnan');
+
+    case {'tanh', 'atanh'}
+        % apply atanh()
+        if any(ismember([max(corr_mat), min(corr_mat)], [-1, 1]))
+            warning(['The output (cor) may not make sense as at least one ' ...
+                'of the correaltions is 1 or -1.']);
+        end
+        cor = tanh(mean(atanh(corr_mat), 1, 'omitnan'));
+
+    case 'raw' % keep what it is
+    otherwise
+        error('The option of %s as <meanout> is not avaiable.')
+end %switch method
+
+end %method
