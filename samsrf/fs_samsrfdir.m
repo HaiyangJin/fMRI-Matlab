67c037790a89e2dfed7f0774329d5a36132ef8ad
Haiyang Jin <hjin317@aucklanduni.ac.nz>
2023-06-29 09:14:44 +0400

    throw warning SAMSRF_DIR is not set up

--- samsrf/fs_samsrfdir.m
+++ samsrf/fs_samsrfdir.m
@@ -6,6 +6,8 @@ function fs_samsrf_setupdir(samsrfDir)
 if ~isempty(samsrfDir) && exist(samsrfDir, 'dir')
     setenv('SAMSRF_DIR', samsrfDir);
     fprintf('$SAMSRF_DIR is set as %s now...\n', samsrfDir);
+else
+    warning('SAMSRF_DIR is not set up.')
 end
 
 end
