f84f372cd7890793bf1230afdcfd13c8de375dd7
Haiyang Jin <hjin317@aucklanduni.ac.nz>
2023-06-29 17:10:45 +0400

    rename the func

--- samsrf/fs_samsrfdir.m
+++ samsrf/fs_samsrfdir.m
@@ -1,11 +1,11 @@
 function fs_samsrfdir(samsrfDir)
-% fs_samsrf_setupdir(samsrfDir)
+% fs_samsrfdir(samsrfDir)
 %
 % Set $SAMSRF_DIR.
 
 if ~isempty(samsrfDir) && exist(samsrfDir, 'dir')
     setenv('SAMSRF_DIR', samsrfDir);
-    fprintf('SAMSRF_DIR is set as %s now...\n', samsrfDir);
+    fprintf('$SAMSRF_DIR is set as %s now...\n', samsrfDir);
 else
     warning('SAMSRF_DIR is not set up.')
 end
