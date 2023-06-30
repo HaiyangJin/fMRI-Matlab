bf5ea97887272c526970446985d976a7e69bf96d
Haiyang Jin <hjin317@aucklanduni.ac.nz>
2023-06-30 17:47:48 +0400

    get SAMSRF_DIR if no input arguemnt

--- samsrf/fs_samsrfdir.m
+++ samsrf/fs_samsrfdir.m
@@ -1,9 +1,11 @@
-function fs_samsrfdir(samsrfDir)
+function samsrfDir = fs_samsrfdir(samsrfDir)
 % fs_samsrfdir(samsrfDir)
 %
 % Set $SAMSRF_DIR.
 
-if ~isempty(samsrfDir) && exist(samsrfDir, 'dir')
+if ~exist('samsrfDir', 'var') 
+    samsrfDir = getenv('SAMSRF_DIR');
+elseif ~isempty(samsrfDir) && exist(samsrfDir, 'dir')
     setenv('SAMSRF_DIR', samsrfDir);
     fprintf('$SAMSRF_DIR is set as %s now...\n', samsrfDir);
 else
